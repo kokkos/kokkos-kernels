57de43659b3356311d3fd7c50f3390519e50f24d
Mark Hoemmen <mhoemme@sandia.gov>
2017-03-21 16:40:23 -0600

    KokkosBlas::fill: Demonstrate how to specialize with "extern template"
    The previous version of KokkosBlas::fill, before Christian's
    KokkosKernel ETI refactor, defined a full specialization for
    Scalar=double, execution_space=Kokkos::Serial in the .cpp file.
    Christian's refactor got rid of the .cpp files in the repository (it
    generates them now), and the full specialization technique doesn't
    work very well with the "extern template" approach.
    
    This commit restores the full specialization for a slightly more
    general case, by using partial specialization with a new default
    template parameter.
    
    I don't have a good way to test this commit in this branch
    (unify_eti).  KokkosKernels currently only builds inside of Trilinos,
    but Christian's refactor changes how ETI works for all of
    KokkosKernels.  Thus, I'm just going to push this commit for now.  Any
    build errors should not be hard to fix.

--- src/impl/Kokkos_Blas1_MV_impl_fill.hpp
+++ src/impl/Kokkos_Blas1_MV_impl_fill.hpp
@@ -46,6 +46,8 @@
 #include <KokkosKernels_config.h>
 #include <Kokkos_Core.hpp>
 #include <Kokkos_InnerProductSpaceTraits.hpp>
+#include <type_traits>
+#include <cstring> // for memset (see KokkosBlas::fill specializations below)
 
 namespace KokkosBlas {
 namespace Impl {
@@ -152,12 +154,37 @@ MV_Fill_Invoke (const XMV& X, const typename XMV::non_const_value_type& val)
 
 /// \brief Implementation of KokkosBlas::fill for multivectors and
 ///   single vectors.
-template<class XMV, int rank = XMV::rank>
+///
+/// \tparam XMV Type of the Kokkos::View to fill.
+/// \tparam rank (Integer) rank of XMV.  1 means a 1-D View, 2 means a
+///   2-D View, etc.
+/// \tparam useMemset Whether to use memset if possible (if the View
+///   occupies a contiguous chunk of memory).  This depends on the
+///   value type in the View, as well as the View's layout, execution
+///   space (we only use memset for Kokkos::Serial, else no
+///   parallelism), and memory space (it must be host-reachable
+///   memory).
+///
+template<class XMV,
+         const int rank = XMV::rank,
+         const bool useMemset =
+#ifdef KOKKOS_HAVE_SERIAL
+           std::is_fundamental<typename XMV::non_const_value_type>::value &&
+           (std::is_integral<typename XMV::non_const_value_type>::value ||
+            std::is_floating_point<typename XMV::non_const_value_type>::value) &&
+           (std::is_same<typename XMV::array_layout, Kokkos::LayoutLeft>::value ||
+            std::is_same<typename XMV::array_layout, Kokkos::LayoutRight>::value) &&
+           std::is_same<typename XMV::memory_space, Kokkos::HostSpace>::value &&
+           std::is_same<typename XMV::execution_space, Kokkos::Serial>::value
+#else // NOT KOKKOS_HAVE_SERIAL
+         false
+#endif // KOKKOS_HAVE_SERIAL
+         >
 struct Fill {};
 
-// Specialization for multivectors (2-D Views).
+// Specialization for multivectors (2-D Views), not using memset at all.
 template<class XMV>
-struct Fill<XMV, 2> {
+struct Fill<XMV, 2, false> {
   static void fill (const XMV& X, const typename XMV::non_const_value_type& val)
   {
     typedef typename XMV::size_type size_type;
@@ -176,9 +203,66 @@ struct Fill<XMV, 2> {
   }
 };
 
-// Specialization for single vectors (1-D Views).
+// Specialization for multivectors (2-D Views), using memset if possible.
+template<class XMV>
+struct Fill<XMV, 2, true> {
+  static void fill (const XMV& X, const typename XMV::non_const_value_type& val)
+  {
+    typedef typename XMV::size_type size_type;
+    const size_type numRows = X.dimension_0 ();
+    const size_type numCols = X.dimension_1 ();
+
+    // Don't call one of the special cases (memset or 1-D fill) unless
+    // the memory in X is contiguous.
+    if (X.capacity () == numRows * numCols) {
+      typedef typename XMV::non_const_value_type SC;
+
+      if (val == Kokkos::Details::ArithTraits<SC>::zero ()) {
+        // It might not necessarily be true for ALL Scalar types that
+        // memset with 0 does the right thing, but it certainly works
+        // here.
+        memset (X.ptr_on_device (), 0, numRows * numCols * sizeof (SC));
+      }
+      else {
+        typedef Kokkos::View<SC*,
+          typename XMV::array_layout,
+          typename XMV::device_type,
+          Kokkos::MemoryTraits<Kokkos::Unmanaged> > XV1D;
+
+        XV1D X1D (X.ptr_on_device (), X.capacity ());
+        Kokkos::deep_copy (X1D, val);
+
+        // mfh 14 Apr 2015: The code below didn't actually help
+        // performance over using ViewFill (mfh 21 Mar 2017: or
+        // Kokkos::deep_copy?) on the 1-D View.  The key thing is
+        // using the right ViewFill specialization: 1-D is faster (.18
+        // s / 26 s, for example).
+
+        // if (numRows < static_cast<size_type> (INT_MAX)) {
+        //   V_Fill_Invoke<XV1D, int> (X1D, val);
+        // }
+        // else {
+        //   V_Fill_Invoke<XV1D, size_type> (X1D, val);
+        // }
+      }
+    }
+    else {
+      // The first condition helps avoid overflow with the
+      // multiplication in the second condition.
+      if (numRows < static_cast<size_type> (INT_MAX) &&
+          numRows * numCols < static_cast<size_type> (INT_MAX)) {
+        MV_Fill_Invoke<XMV, int> (X, val);
+      }
+      else {
+        MV_Fill_Invoke<XMV, size_type> (X, val);
+      }
+    }
+  }
+};
+
+// Specialization for single vectors (1-D Views), not using memset at all.
 template<class XV>
-struct Fill<XV, 1> {
+struct Fill<XV, 1, false> {
   static void fill (const XV& X, const typename XV::non_const_value_type& val)
   {
     typedef typename XV::size_type size_type;
@@ -189,6 +273,24 @@ struct Fill<XV, 1> {
     }
     else {
       V_Fill_Invoke<XV, size_type> (X, val);
+    }
+  }
+};
+
+// Specialization for single vectors (1-D Views), using memset if possible.
+template<class XV>
+struct Fill<XV, 1, true> {
+  static void fill (const XV& X, const typename XV::non_const_value_type& val)
+  {
+    typedef typename XMV::non_const_value_type SC;
+    typedef typename XV::size_type size_type;
+    const size_type numRows = X.dimension_0 ();
+
+    if (val == Kokkos::Details::ArithTraits<SC>::zero ()) {
+      memset (X.ptr_on_device (), 0, numRows * sizeof (SC));
+    }
+    else {
+      Kokkos::deep_copy (X, val);
     }
   }
 };
