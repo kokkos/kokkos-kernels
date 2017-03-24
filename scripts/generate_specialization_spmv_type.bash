e918ac5de1acbb083fda9520f3d2d9635e170e60
crtrott <crtrott@sandia.gov>
2017-03-24 14:35:19 -0600

    ETI: fix generate script for spmv

--- scripts/generate_specialization_spmv_type.bash
+++ scripts/generate_specialization_spmv_type.bash
@@ -31,7 +31,7 @@ echo "#if defined (KOKKOSKERNELS_INST_SCALAR_${Scalar_UpperCase}) \\" >> ${filen
 echo " && defined (KOKKOSKERNELS_INST_LAYOUT_${Layout_UpperCase}) \\" >> ${filename_cpp} 
 echo " && defined (KOKKOSKERNELS_INST_EXECSPACE_${ExecSpace_UpperCase}) \\" >> ${filename_cpp} 
 echo " && defined (KOKKOSKERNELS_INST_EXECSPACE_${MemSpace_UpperCase})" >> ${filename_cpp} 
-echo " ${Macro}_DEF(${Scalar}, Kokkos::${Layout}, ${OffsetType}, Kokkos::${ExecSpace}, Kokkos::${MemSpace})" >> ${filename_cpp}
+echo " ${Macro}_DEF(${Scalar}, int, ${OffsetType}, Kokkos::${Layout}, Kokkos::${ExecSpace}, Kokkos::${MemSpace})" >> ${filename_cpp}
 echo "#endif" >> ${filename_cpp}
 echo "} // Impl" >> ${filename_cpp} 
 echo "} // ${NameSpace}" >> ${filename_cpp}
@@ -41,5 +41,5 @@ echo "#if defined (KOKKOSKERNELS_INST_SCALAR_${Scalar_UpperCase}) \\" >> ${filen
 echo " && defined (KOKKOSKERNELS_INST_LAYOUT_${Layout_UpperCase}) \\" >> ${filename_hpp}
 echo " && defined (KOKKOSKERNELS_INST_EXECSPACE_${ExecSpace_UpperCase}) \\" >> ${filename_hpp}
 echo " && defined (KOKKOSKERNELS_INST_EXECSPACE_${MemSpace_UpperCase})" >> ${filename_hpp}
-echo " ${Macro}_DECL(${Scalar}, Kokkos::${Layout}, ${OffsetType}, Kokkos::${ExecSpace}, Kokkos::${MemSpace})" >> ${filename_hpp}
+echo " ${Macro}_DECL(${Scalar}, int, ${OffsetType}, Kokkos::${Layout}, Kokkos::${ExecSpace}, Kokkos::${MemSpace})" >> ${filename_hpp}
 echo "#endif" >> ${filename_hpp}
