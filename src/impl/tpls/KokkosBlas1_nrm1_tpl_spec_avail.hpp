8748efb6f8f39cfee3dd75772dd4d4c25125f756
crtrott <crtrott@sandia.gov>
2017-05-26 14:33:20 -0600

    ETI-FileLayout: Reworked the nrm1 function

--- src/impl/tpls/KokkosBlas1_nrm1_tpl_spec_avail.hpp
+++ src/impl/tpls/KokkosBlas1_nrm1_tpl_spec_avail.hpp
@@ -41,12 +41,42 @@
 //@HEADER
 */
 
-#include<KokkosBlas1_abs.hpp>
-#include<KokkosBlas1_axpby.hpp>
-#include<KokkosBlas1_dot.hpp>
-#include<KokkosBlas1_nrm1.hpp>
-#include<KokkosBlas1_nrm2.hpp>
-#include<KokkosBlas1_nrm2_squared.hpp>
-#include<KokkosBlas1_nrminf.hpp>
-#include<KokkosBlas1_scal.hpp>
-#include<KokkosBlas1_sum.hpp>
+#ifndef KOKKOSBLAS1_NRM1_TPL_SPEC_AVAIL_HPP_
+#define KOKKOSBLAS1_NRM1_TPL_SPEC_AVAIL_HPP_
+
+namespace KokkosBlas {
+namespace Impl {
+// Specialization struct which defines whether a specialization exists
+template<class AV, class XMV, int Xrank = XMV::Rank>
+struct nrm1_tpl_spec_avail {
+  enum : bool { value = false };
+};
+}
+}
+
+namespace KokkosBlas {
+namespace Impl {
+
+// Generic Host side BLAS (could be MKL or whatever)
+#ifdef KOKKOSKERNELS_ENABLE_TPL_BLAS
+// double
+#define KOKKOSBLAS1_NRM1_TPL_SPEC_AVAIL_BLAS( SCALAR, LAYOUT, MEMSPACE ) \
+template<class ExecSpace> \
+struct nrm1_tpl_spec_avail< \
+Kokkos::View<typename Kokkos::Details::InnerProductSpaceTraits<SCALAR>::mag_type, LAYOUT, Kokkos::HostSpace, \
+             Kokkos::MemoryTraits<Kokkos::Unmanaged> >, \
+Kokkos::View<const SCALAR*, LAYOUT, Kokkos::Device<ExecSpace, MEMSPACE>, \
+             Kokkos::MemoryTraits<Kokkos::Unmanaged> >, \
+1> { enum : bool { value = true }; };
+
+KOKKOSBLAS1_NRM1_TPL_SPEC_AVAIL_BLAS( double,                  Kokkos::LayoutLeft, Kokkos::HostSpace)
+KOKKOSBLAS1_NRM1_TPL_SPEC_AVAIL_BLAS( float,                   Kokkos::LayoutLeft, Kokkos::HostSpace)
+KOKKOSBLAS1_NRM1_TPL_SPEC_AVAIL_BLAS( Kokkos::complex<double>, Kokkos::LayoutLeft, Kokkos::HostSpace)
+KOKKOSBLAS1_NRM1_TPL_SPEC_AVAIL_BLAS( Kokkos::complex<float>,  Kokkos::LayoutLeft, Kokkos::HostSpace)
+
+
+#endif
+
+}
+}
+#endif
