b45b94eb285c102d50f47cfc69935ebfd89d6c1f
crtrott <crtrott@sandia.gov>
2017-06-02 13:07:12 -0600

    UnitTests: adding cmake option to only build the tests for ETI types
    This is on by default

--- unit_test/threads/Test_Threads.hpp
+++ unit_test/threads/Test_Threads.hpp
@@ -1,5 +1,10 @@
 #include <gtest/gtest.h>
 #include <Kokkos_Core.hpp>
+#include <KokkosKernels_config.h>
+
+#if defined(KOKKOSKERNELS_TEST_ETI_ONLY) && !defined(KOKKOSKERNELS_ETI_ONLY)
+#define KOKKOSKERNELS_ETI_ONLY
+#endif
 
 class threads : public ::testing::Test {
 protected:
