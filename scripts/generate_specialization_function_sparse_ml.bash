e456a1e63a49abadf7fded68abbc03af13f06eb7
Mehmet Deveci <mndevec@sandia.gov>
2017-09-18 12:13:14 -0600

    This commit completes the multi-level memory system ETI and compilation.
    On KNLs, to enable explicit HBM and DDR management: run generate makefile with:
    --with-memkind=/home/projects/x86-64-knl/memkind/20160811/gcc/4.8.3/
    
    This will add HBWSpace to kokkos devices, and ETI'ed functions will instantiate
    for both HostSpace and HBWSpace. I updated these for all blas and sparse functions,
    because HBWSpace becomes the default memory space when memkind is enabled, and
    instantiating HBWSpace becomes necessity.
    
    For GPUs, if you set KOKKOS_DEVICES=CudaHostPinnedSpace,...
    This will generate ETI functions for this memory space as well.
    This is only added to sparse functions. This is an optional space,
    and "NOT Necessary" to instantiate for blas kernels. I did not add
    them for this reason.

--- scripts/generate_specialization_function_sparse_ml.bash
+++ scripts/generate_specialization_function_sparse_ml.bash
@@ -9,7 +9,7 @@ OrdinalList="int int64_t"
 OffsetList="int size_t"
 ScalarList="double float Kokkos::complex<double> Kokkos::complex<float>"
 LayoutList="LayoutLeft LayoutRight"
-ExecMemSpaceList="Cuda,CudaSpace,CudaSpace Cuda,CudaUVMSpace,CudaUVMSpace OpenMP,HostSpace,HostSpace Threads,HostSpace,HostSpace Serial,HostSpace,HostSpace OpenMP,Experimental::HBWSpace,Experimental::HBWSpace Threads,Experimental::HBWSpace,Experimental::HBWSpace Serial,Experimental::HBWSpace,Experimental::HBWSpace Cuda,CudaSpace,CudaHostPinnedSpace Cuda,CudaUVMSpace,CudaUVMSpace OpenMP,Experimental::HBWSpace,HostSpace Threads,Experimental::HBWSpace,HostSpace Serial,Experimental::HBWSpace,HostSpace"
+ExecMemSpaceList="Cuda,CudaSpace,CudaSpace Cuda,CudaUVMSpace,CudaUVMSpace OpenMP,HostSpace,HostSpace Threads,HostSpace,HostSpace Serial,HostSpace,HostSpace OpenMP,Experimental::HBWSpace,Experimental::HBWSpace Threads,Experimental::HBWSpace,Experimental::HBWSpace Serial,Experimental::HBWSpace,Experimental::HBWSpace Cuda,CudaSpace,CudaHostPinnedSpace Cuda,CudaHostPinnedSpace,CudaHostPinnedSpace Cuda,CudaUVMSpace,CudaUVMSpace OpenMP,Experimental::HBWSpace,HostSpace Threads,Experimental::HBWSpace,HostSpace Serial,Experimental::HBWSpace,HostSpace"
 
 mkdir generated_specializations_hpp
 mkdir generated_specializations_cpp/${Function}
