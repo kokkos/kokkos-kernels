bd8511ce69c2d86e6d009364a9a0b02fd7791d94
Kyungjoo Kim <kyukim@sandia.gov>
2018-02-28 11:50:51 -0700

    KokkoBatched - clean up ; compiled and tested on my workstation

--- src/batched/KokkosBatched_Vector.hpp
+++ src/batched/KokkosBatched_Vector.hpp
@@ -5,26 +5,25 @@
 
 #include "KokkosBatched_Util.hpp"
 
+
+// forward declaration
 namespace KokkosBatched {
   namespace Experimental {
     template<typename T, int l>
     class Vector;
     
     template<typename T, int l> struct is_vector<Vector<SIMD<T>,l> > : public std::true_type {};
-    template<typename T, int l> struct is_vector<Vector<AVX<T>,l> >  : public std::true_type {};
   }
 }
 
 #include "KokkosBatched_Vector_SIMD.hpp"
-#include "KokkosBatched_Vector_AVX256D.hpp"
-#include "KokkosBatched_Vector_AVX512D.hpp"
 
+// arith traits overload for vector types
 namespace Kokkos {
   namespace Details {
 
     using namespace KokkosBatched::Experimental;
 
-    // this is later, arith traits may not necessary to be overloaded for simd type
     template<typename T, int l>
     class ArithTraits<Vector<SIMD<T>,l> > { 
     public:
@@ -33,18 +32,6 @@ namespace Kokkos {
 
       typedef Vector<SIMD<val_scalar_type>,l> val_type;
       typedef Vector<SIMD<mag_scalar_type>,l> mag_type;
-      
-      static const bool is_specialized = ArithTraits<T>::is_specialized;
-      static const bool is_signed = ArithTraits<T>::is_signed;
-      static const bool is_integer = ArithTraits<T>::is_integer;
-      static const bool is_exact = ArithTraits<T>::is_exact;
-      static const bool is_complex = ArithTraits<T>::is_complex;
-    };
-    template<typename T, int l>
-    class ArithTraits<Vector<AVX<T>,l> > { 
-    public:
-      typedef typename ArithTraits<T>::val_type val_type;
-      typedef typename ArithTraits<T>::mag_type mag_type;
       
       static const bool is_specialized = ArithTraits<T>::is_specialized;
       static const bool is_signed = ArithTraits<T>::is_signed;
