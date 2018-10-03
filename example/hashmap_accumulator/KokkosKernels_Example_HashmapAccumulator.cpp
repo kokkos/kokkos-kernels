b7b39a0d9c5c7f4af736225d3636a1390f7152c6
William McLendon <wcmclen@sandia.gov>
2018-10-03 10:56:58 -0600

    Update contact information for Hashmap Accumulator example

--- example/hashmap_accumulator/KokkosKernels_Example_HashmapAccumulator.cpp
+++ example/hashmap_accumulator/KokkosKernels_Example_HashmapAccumulator.cpp
@@ -35,7 +35,8 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
-// Questions Contact  H. Carter Edwards (hcedwar@sandia.gov)
+// Questions Contact: William McLendon (wcmclen@sandia.gov) or
+//                    Siva Rajamanickam (srajama@sandia.gov)
 //
 // ************************************************************************
 //@HEADER
