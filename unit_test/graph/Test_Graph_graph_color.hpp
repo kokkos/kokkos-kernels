a2e266aa260d62318f08b1a1c296748e0d5452e9
William McLendon <wcmclen@sandia.gov>
2018-10-09 17:31:11 -0600

    Graph coloring test fixes for COLORING_VBD
    Test_Graph_graph_color_deterministic also needs the guards around
    CUDA for COLORING_VBD.

--- unit_test/graph/Test_Graph_graph_color.hpp
+++ unit_test/graph/Test_Graph_graph_color.hpp
@@ -124,24 +124,25 @@ void test_coloring(lno_t numRows,size_type nnz, lno_t bandwidth, lno_t row_size_
   graph_t static_graph (sym_adj, sym_xadj);
   input_mat = crsMat_t("CrsMatrix", numCols, newValues, static_graph);
 
-  std::vector<ColoringAlgorithm> coloring_algorithms = { COLORING_DEFAULT, 
-                                                         COLORING_SERIAL, 
-                                                         COLORING_VB, 
-                                                         COLORING_VBBIT, 
-                                                         COLORING_VBCS, 
-                                                         COLORING_EB, 
-                                                         COLORING_VBDBIT };
+  std::vector<ColoringAlgorithm> coloring_algorithms = { COLORING_DEFAULT 
+                                                       , COLORING_SERIAL 
+                                                       , COLORING_VB 
+                                                       , COLORING_VBBIT 
+                                                       , COLORING_VBCS 
+                                                       , COLORING_EB
+                                                       , COLORING_VBDBIT
+                                                       };
 
-#ifdef KOKKOS_ENABLE_CUDA
+  #ifdef KOKKOS_ENABLE_CUDA
   if( !std::is_same< typename device::execution_space, Kokkos::Cuda >::value )
   {
     coloring_algorithms.push_back(COLORING_VBD);
   }
-#else
+  #else
   coloring_algorithms.push_back(COLORING_VBD);
-#endif
+  #endif
 
-  for (size_t ii = 0; ii < coloring_algorithms.size(); ++ii){
+  for (size_t ii = 0; ii < coloring_algorithms.size(); ++ii) {
     ColoringAlgorithm coloring_algorithm = coloring_algorithms[ii];
     color_view_t vector_colors;
     size_t num_colors;
