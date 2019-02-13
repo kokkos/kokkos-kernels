87eea3f0d0da89901f211a4b10c20799936eae90
Kyungjoo Kim <kyukim@sandia.gov>
2019-02-13 16:15:01 -0700

    KokkosBatched - add missing files

--- src/batched/KokkosBatched_SetIdentity_Internal.hpp
+++ src/batched/KokkosBatched_SetIdentity_Internal.hpp
@@ -32,6 +32,31 @@ namespace KokkosBatched {
       }
     };
 
+    ///
+    /// Team Internal Impl
+    /// ==================
+    template<typename MemberType>
+    struct TeamSetIdentityInternal {
+      template<typename ValueType>
+      KOKKOS_INLINE_FUNCTION
+      static int
+      invoke(MemberType const int m, 
+             /* */ ValueType *__restrict__ A, const int as0, const int as1) {
+        const ValueType one(1), zero(0);
+        Kokkos::parallel_for
+          (Kokkos::TeamThreadRange(member,0,m),
+           [&](const int &i) {
+#if defined(KOKKOS_ENABLE_PRAGMA_UNROLL)
+#pragma unroll
+#endif
+        for (int j=0;j<m;++j) 
+          A[i*as0+j*as1] = i == j ? one : zero;
+          });
+        
+        return 0;
+      }
+    };
+
   }//  end namespace Experimental
 } // end namespace KokkosBatched
 
