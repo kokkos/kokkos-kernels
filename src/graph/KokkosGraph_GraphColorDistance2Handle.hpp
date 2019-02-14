46bceaa2cbc1cbc126fe3016d92f0c5368cf7584
William McLendon <wcmclen@sandia.gov>
2019-02-13 18:08:50 -0700

    D2GC: Changed contact info in header block to me

--- src/graph/KokkosGraph_GraphColorDistance2Handle.hpp
+++ src/graph/KokkosGraph_GraphColorDistance2Handle.hpp
@@ -35,7 +35,7 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
-// Questions? Contact Siva Rajamanickam (srajama@sandia.gov)
+// Questions? Contact William McLendon (wcmclen@sandia.gov)
 //
 // ************************************************************************
 //@HEADER
