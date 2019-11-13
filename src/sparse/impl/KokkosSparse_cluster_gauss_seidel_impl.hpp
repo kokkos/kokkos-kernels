73dc833f250d73f38bb0ed5fc3e8f7e3fcf05059
Brian Kelley <bmkelle@sandia.gov>
2019-11-13 14:51:37 -0700

    Fixed type mismatch error

--- src/sparse/impl/KokkosSparse_cluster_gauss_seidel_impl.hpp
+++ src/sparse/impl/KokkosSparse_cluster_gauss_seidel_impl.hpp
@@ -515,7 +515,7 @@ namespace KokkosSparse{
           unsigned h = xorshiftHash(nei);
           for(unsigned i = h; i < h + 2; i++)
           {
-            if(Kokkos::atomic_compare_exchange_strong(&table[i % tableSize()], cluster, nei))
+            if(Kokkos::atomic_compare_exchange_strong<int>(&table[i % tableSize()], cluster, nei))
               return true;
           }
           return false;
