f57e99a5bb80a4051c2f735dae6d2a9da484bfaa
Brian Kelley <bmkelle@sandia.gov>
2020-04-20 15:31:54 -0600

    Actually remove the decl of unused variable

--- src/batched/KokkosBatched_Eigendecomposition_Serial_Internal.hpp
+++ src/batched/KokkosBatched_Eigendecomposition_Serial_Internal.hpp
@@ -67,7 +67,6 @@ namespace KokkosBatched {
       assert( (wlen_now >= 0) && "Eigendecomposition: workspace size is negative");
 
       const bool is_UL = UL != NULL, is_UR = UR != NULL;
-      const bool is_U  = is_UL || is_UR;
       assert((is_UL || is_UR) && "Eigendecomposition: neither left nor right eigenvectors were requested. Use SerialEigenvalueInternal instead.");
         
       real_type *QZ = w_now; w_now += (m*m); wlen_now -= (m*m);
