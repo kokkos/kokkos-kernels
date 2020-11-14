8ca31c45d03162142baae90b045b737d0ad3c97c
Evan Harvey <eharvey@sandia.gov>
2020-11-13 19:20:20 -0800

    Add half_t explicit conversions

--- unit_test/batched/Test_Batched_SerialGemm.hpp
+++ unit_test/batched/Test_Batched_SerialGemm.hpp
@@ -83,7 +83,8 @@ template<typename DeviceType,
     using ats = Kokkos::Details::ArithTraits<host_value_type>;
 
     /// randomized input testing views
-    ScalarType alpha = 1.5, beta = 3.0;
+    ScalarType alpha = ScalarType(1.5);
+    ScalarType beta = ScalarType(3.0);
 
     ViewType
       a_expected("a_expected", N, matAdim1, matAdim2), a1("a1", N, matAdim1, matAdim2),
