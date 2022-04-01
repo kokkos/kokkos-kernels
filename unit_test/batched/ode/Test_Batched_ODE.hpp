7bfac28c9293bc5f91cbd324dc9d4916dbd23fb5
Jennifer Loe <jloe@sandia.gov>
2022-04-01 11:40:01 -0600

    Fix naming of unit test files.

--- unit_test/batched/ode/Test_Batched_ODE.hpp
+++ unit_test/batched/ode/Test_Batched_ODE.hpp
@@ -1,7 +1,7 @@
 #ifndef TEST_BATCHED_ODE_HPP
 #define TEST_BATCHED_ODE_HPP
 
-#include <Test_Batched_ODE_Dummy.hpp> 
-#include <Test_Batched_ODE_Perf.hpp> 
+#include <Test_Batched_ODE_Solve.hpp> 
+#include <Test_Batched_ODE_Performance.hpp> 
 
 #endif  // TEST_BATCHED_ODE_HPP
