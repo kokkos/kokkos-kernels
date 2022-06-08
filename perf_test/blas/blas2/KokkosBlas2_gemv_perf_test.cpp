a70474ce0030a079edefe17da9155625b869fb87
Luc Berger-Vergiat <lberge@sandia.gov>
2022-06-08 11:39:49 -0600

    Test clean-up: removing unnecessary include from KokkosBlas2_gemv_perf_test.cpp

--- perf_test/blas/blas2/KokkosBlas2_gemv_perf_test.cpp
+++ perf_test/blas/blas2/KokkosBlas2_gemv_perf_test.cpp
@@ -45,7 +45,6 @@
 #include "KokkosBlas2_gemv.hpp"
 #include <Kokkos_Random.hpp>
 #include "KokkosKernels_TestUtils.hpp"
-#include "KokkosKernels_IOUtils.hpp"
 
 struct Params {
   int use_cuda    = 0;
