969e1a56d4d31c61cf40fec4a1496c14a4c53cf5
Mikołaj Zuzek <mikolaj.zuzek@ng-analytics.com>
2022-06-28 23:29:13 +0200

    Move {Serial,Team}{Set,Scale} unit tests from KokkosBatched to KokkosBlas

--- unit_test/blas/Test_Blas.hpp
+++ unit_test/blas/Test_Blas.hpp
@@ -23,7 +23,11 @@
 #include "Test_Blas1_sum.hpp"
 #include "Test_Blas1_update.hpp"
 
+// Serial Blas 1
+#include "Test_Blas1_serial_setscal.hpp"
+
 // Team Blas 1
+#include "Test_Blas1_team_setscal.hpp"
 #include "Test_Blas1_team_abs.hpp"
 #include "Test_Blas1_team_axpby.hpp"
 #include "Test_Blas1_team_axpy.hpp"
