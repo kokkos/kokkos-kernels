51185f3fd32a9dd89f76d3c33a31d70cc8f6a7e8
Mikołaj Zuzek <mikolaj.zuzek@ng-analytics.com>
2022-09-09 16:25:00 +0200

    GEMM: move MKL implementation of SerialGemm to dedicated TPL header

--- blas/impl/KokkosBlas3_serial_gemm_impl.hpp
+++ blas/impl/KokkosBlas3_serial_gemm_impl.hpp
@@ -46,6 +46,7 @@
 #define KOKKOSBLAS3_SERIAL_GEMM_IMPL_HPP_
 
 #include "KokkosBlas3_serial_gemm_internal.hpp"
+#include "KokkosBlas3_serial_gemm_tpl_spec_decl.hpp"
 #include "KokkosBlas_util.hpp"
 
 namespace KokkosBlas {
@@ -65,52 +66,6 @@ namespace KokkosBlas {
 ///
 /// NT/NT
 ///
-#if defined(__KOKKOSBATCHED_ENABLE_INTEL_MKL__) &&         \
-    defined(__KOKKOSBATCHED_ENABLE_INTEL_MKL_BATCHED__) && \
-    defined(__KOKKOSBATCHED_ENABLE_INTEL_MKL_COMPACT_BATCHED__)
-template <>
-template <typename ScalarType, typename AViewType, typename BViewType,
-          typename CViewType>
-KOKKOS_INLINE_FUNCTION int
-SerialGemm<Trans::NoTranspose, Trans::NoTranspose,
-           Algo::Gemm::CompactMKL>::invoke(const ScalarType alpha,
-                                           const AViewType &A,
-                                           const BViewType &B,
-                                           const ScalarType beta,
-                                           const CViewType &C) {
-  typedef typename CViewType::value_type vector_type;
-  // typedef typename vector_type::value_type value_type;
-
-  const int m = C.extent(0), n = C.extent(1), k = A.extent(1);
-
-  static_assert(KokkosBatched::is_vector<vector_type>::value,
-                "value type is not vector type");
-  static_assert(
-      vector_type::vector_length == 4 || vector_type::vector_length == 8,
-      "AVX, AVX2 and AVX512 is supported");
-  const MKL_COMPACT_PACK format =
-      vector_type::vector_length == 8 ? MKL_COMPACT_AVX512 : MKL_COMPACT_AVX;
-
-  // no error check
-  int r_val = 0;
-  if (A.stride_0() == 1 && B.stride_0() == 1 && C.stride_0() == 1) {
-    mkl_dgemm_compact(MKL_COL_MAJOR, MKL_NOTRANS, MKL_NOTRANS, m, n, k, alpha,
-                      (const double *)A.data(), A.stride_1(),
-                      (const double *)B.data(), B.stride_1(), beta,
-                      (double *)C.data(), C.stride_1(), format,
-                      (MKL_INT)vector_type::vector_length);
-  } else if (A.stride_1() == 1 && B.stride_1() == 1 && C.stride_1() == 1) {
-    mkl_dgemm_compact(MKL_ROW_MAJOR, MKL_NOTRANS, MKL_NOTRANS, m, n, k, alpha,
-                      (const double *)A.data(), A.stride_0(),
-                      (const double *)B.data(), B.stride_0(), beta,
-                      (double *)C.data(), C.stride_0(), format,
-                      (MKL_INT)vector_type::vector_length);
-  } else {
-    r_val = -1;
-  }
-  return r_val;
-}
-#endif
 
 template <>
 template <typename ScalarType, typename AViewType, typename BViewType,
@@ -149,53 +104,6 @@ SerialGemm<Trans::NoTranspose, Trans::NoTranspose, Algo::Gemm::Blocked>::invoke(
 /// T/NT
 ///
 
-#if defined(__KOKKOSBATCHED_ENABLE_INTEL_MKL__) &&         \
-    defined(__KOKKOSBATCHED_ENABLE_INTEL_MKL_BATCHED__) && \
-    defined(__KOKKOSBATCHED_ENABLE_INTEL_MKL_COMPACT_BATCHED__)
-template <>
-template <typename ScalarType, typename AViewType, typename BViewType,
-          typename CViewType>
-KOKKOS_INLINE_FUNCTION int
-SerialGemm<Trans::Transpose, Trans::NoTranspose,
-           Algo::Gemm::CompactMKL>::invoke(const ScalarType alpha,
-                                           const AViewType &A,
-                                           const BViewType &B,
-                                           const ScalarType beta,
-                                           const CViewType &C) {
-  typedef typename CViewType::value_type vector_type;
-  // typedef typename vector_type::value_type value_type;
-
-  const int m = C.extent(0), n = C.extent(1), k = A.extent(0);
-
-  static_assert(KokkosBatched::is_vector<vector_type>::value,
-                "value type is not vector type");
-  static_assert(
-      vector_type::vector_length == 4 || vector_type::vector_length == 8,
-      "AVX, AVX2 and AVX512 is supported");
-  const MKL_COMPACT_PACK format =
-      vector_type::vector_length == 8 ? MKL_COMPACT_AVX512 : MKL_COMPACT_AVX;
-
-  // no error check
-  int r_val = 0;
-  if (A.stride_0() == 1 && B.stride_0() == 1 && C.stride_0() == 1) {
-    mkl_dgemm_compact(MKL_COL_MAJOR, MKL_TRANS, MKL_NOTRANS, m, n, k, alpha,
-                      (const double *)A.data(), A.stride_1(),
-                      (const double *)B.data(), B.stride_1(), beta,
-                      (double *)C.data(), C.stride_1(), format,
-                      (MKL_INT)vector_type::vector_length);
-  } else if (A.stride_1() == 1 && B.stride_1() == 1 && C.stride_1() == 1) {
-    mkl_dgemm_compact(MKL_ROW_MAJOR, MKL_TRANS, MKL_NOTRANS, m, n, k, alpha,
-                      (const double *)A.data(), A.stride_0(),
-                      (const double *)B.data(), B.stride_0(), beta,
-                      (double *)C.data(), C.stride_0(), format,
-                      (MKL_INT)vector_type::vector_length);
-  } else {
-    r_val = -1;
-  }
-  return r_val;
-}
-#endif
-
 template <>
 template <typename ScalarType, typename AViewType, typename BViewType,
           typename CViewType>
@@ -230,53 +138,6 @@ SerialGemm<Trans::Transpose, Trans::NoTranspose, Algo::Gemm::Blocked>::invoke(
 /// NT/T
 ///
 
-#if defined(__KOKKOSBATCHED_ENABLE_INTEL_MKL__) &&         \
-    defined(__KOKKOSBATCHED_ENABLE_INTEL_MKL_BATCHED__) && \
-    defined(__KOKKOSBATCHED_ENABLE_INTEL_MKL_COMPACT_BATCHED__)
-template <>
-template <typename ScalarType, typename AViewType, typename BViewType,
-          typename CViewType>
-KOKKOS_INLINE_FUNCTION int
-SerialGemm<Trans::NoTranspose, Trans::Transpose,
-           Algo::Gemm::CompactMKL>::invoke(const ScalarType alpha,
-                                           const AViewType &A,
-                                           const BViewType &B,
-                                           const ScalarType beta,
-                                           const CViewType &C) {
-  typedef typename CViewType::value_type vector_type;
-  // typedef typename vector_type::value_type value_type;
-
-  const int m = C.extent(0), n = C.extent(1), k = A.extent(1);
-
-  static_assert(KokkosBatched::is_vector<vector_type>::value,
-                "value type is not vector type");
-  static_assert(
-      vector_type::vector_length == 4 || vector_type::vector_length == 8,
-      "AVX, AVX2 and AVX512 is supported");
-  const MKL_COMPACT_PACK format =
-      vector_type::vector_length == 8 ? MKL_COMPACT_AVX512 : MKL_COMPACT_AVX;
-
-  // no error check
-  int r_val = 0;
-  if (A.stride_0() == 1 && B.stride_0() == 1 && C.stride_0() == 1) {
-    mkl_dgemm_compact(MKL_COL_MAJOR, MKL_NOTRANS, MKL_TRANS, m, n, k, alpha,
-                      (const double *)A.data(), A.stride_1(),
-                      (const double *)B.data(), B.stride_1(), beta,
-                      (double *)C.data(), C.stride_1(), format,
-                      (MKL_INT)vector_type::vector_length);
-  } else if (A.stride_1() == 1 && B.stride_1() == 1 && C.stride_1() == 1) {
-    mkl_dgemm_compact(MKL_ROW_MAJOR, MKL_NOTRANS, MKL_TRANS, m, n, k, alpha,
-                      (const double *)A.data(), A.stride_0(),
-                      (const double *)B.data(), B.stride_0(), beta,
-                      (double *)C.data(), C.stride_0(), format,
-                      (MKL_INT)vector_type::vector_length);
-  } else {
-    r_val = -1;
-  }
-  return r_val;
-}
-#endif
-
 template <>
 template <typename ScalarType, typename AViewType, typename BViewType,
           typename CViewType>
@@ -310,50 +171,6 @@ SerialGemm<Trans::NoTranspose, Trans::Transpose, Algo::Gemm::Blocked>::invoke(
 ///
 /// T/T
 ///
-
-#if defined(__KOKKOSBATCHED_ENABLE_INTEL_MKL__) &&         \
-    defined(__KOKKOSBATCHED_ENABLE_INTEL_MKL_BATCHED__) && \
-    defined(__KOKKOSBATCHED_ENABLE_INTEL_MKL_COMPACT_BATCHED__)
-template <>
-template <typename ScalarType, typename AViewType, typename BViewType,
-          typename CViewType>
-KOKKOS_INLINE_FUNCTION int
-SerialGemm<Trans::Transpose, Trans::Transpose, Algo::Gemm::CompactMKL>::invoke(
-    const ScalarType alpha, const AViewType &A, const BViewType &B,
-    const ScalarType beta, const CViewType &C) {
-  typedef typename CViewType::value_type vector_type;
-  // typedef typename vector_type::value_type value_type;
-
-  const int m = C.extent(0), n = C.extent(1), k = A.extent(0);
-
-  static_assert(KokkosBatched::is_vector<vector_type>::value,
-                "value type is not vector type");
-  static_assert(
-      vector_type::vector_length == 4 || vector_type::vector_length == 8,
-      "AVX, AVX2 and AVX512 is supported");
-  const MKL_COMPACT_PACK format =
-      vector_type::vector_length == 8 ? MKL_COMPACT_AVX512 : MKL_COMPACT_AVX;
-
-  // no error check
-  int r_val = 0;
-  if (A.stride_0() == 1 && B.stride_0() == 1 && C.stride_0() == 1) {
-    mkl_dgemm_compact(MKL_COL_MAJOR, MKL_TRANS, MKL_TRANS, m, n, k, alpha,
-                      (const double *)A.data(), A.stride_1(),
-                      (const double *)B.data(), B.stride_1(), beta,
-                      (double *)C.data(), C.stride_1(), format,
-                      (MKL_INT)vector_type::vector_length);
-  } else if (A.stride_1() == 1 && B.stride_1() == 1 && C.stride_1() == 1) {
-    mkl_dgemm_compact(MKL_ROW_MAJOR, MKL_TRANS, MKL_TRANS, m, n, k, alpha,
-                      (const double *)A.data(), A.stride_0(),
-                      (const double *)B.data(), B.stride_0(), beta,
-                      (double *)C.data(), C.stride_0(), format,
-                      (MKL_INT)vector_type::vector_length);
-  } else {
-    r_val = -1;
-  }
-  return r_val;
-}
-#endif
 
 template <>
 template <typename ScalarType, typename AViewType, typename BViewType,
