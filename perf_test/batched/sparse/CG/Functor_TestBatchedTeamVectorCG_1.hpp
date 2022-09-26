d4c2168e3ea53f1904c06d925f3619daea57140a
kliegeois <kimliegeois@ymail.com>
2022-09-26 09:43:15 -0600

    Add main functions for batched sparse solver performance tests

---
+++ perf_test/batched/sparse/CG/Functor_TestBatchedTeamVectorCG_1.hpp
@@ -0,0 +1,124 @@
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 3.4
+//       Copyright (2021) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY NTESS "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL NTESS OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact Siva Rajamanickam (srajama@sandia.gov)
+//
+// ************************************************************************
+//@HEADER
+
+template <typename DeviceType, typename ValuesViewType, typename IntView,
+          typename VectorViewType, typename KrylovHandleType>
+struct Functor_TestBatchedTeamVectorCG_1 {
+  const ValuesViewType _D;
+  const IntView _r;
+  const IntView _c;
+  const VectorViewType _X;
+  const VectorViewType _B;
+  const int _N_team, _team_size, _vector_length;
+  KrylovHandleType _handle;
+
+  KOKKOS_INLINE_FUNCTION
+  Functor_TestBatchedTeamVectorCG_1(const ValuesViewType &D, const IntView &r,
+                                    const IntView &c, const VectorViewType &X,
+                                    const VectorViewType &B, const int N_team,
+                                    const int team_size,
+                                    const int vector_length,
+                                    KrylovHandleType &handle)
+      : _D(D),
+        _r(r),
+        _c(c),
+        _X(X),
+        _B(B),
+        _N_team(N_team),
+        _team_size(team_size),
+        _vector_length(vector_length),
+        _handle(handle) {}
+
+  template <typename MemberType>
+  KOKKOS_INLINE_FUNCTION void operator()(const MemberType &member) const {
+    const int first_matrix = _handle.first_index(member.league_rank());
+    const int last_matrix  = _handle.last_index(member.league_rank());
+
+    auto d = Kokkos::subview(_D, Kokkos::make_pair(first_matrix, last_matrix),
+                             Kokkos::ALL);
+    auto x = Kokkos::subview(_X, Kokkos::make_pair(first_matrix, last_matrix),
+                             Kokkos::ALL);
+    auto b = Kokkos::subview(_B, Kokkos::make_pair(first_matrix, last_matrix),
+                             Kokkos::ALL);
+
+    using Operator = KokkosBatched::CrsMatrix<ValuesViewType, IntView>;
+
+    Operator A(d, _r, _c);
+
+    KokkosBatched::TeamVectorCG<MemberType>::template invoke<Operator,
+                                                             VectorViewType>(
+        member, A, b, x, _handle);
+  }
+
+  inline double run() {
+    std::string name("KokkosBatched::Test::TeamCG");
+    Kokkos::Timer timer;
+    Kokkos::Profiling::pushRegion(name.c_str());
+
+    _handle.set_memory_strategy(1);
+
+    _handle.tmp_view = typename KrylovHandleType::TemporaryViewType(
+        "", _X.extent(0), 4 * _X.extent(1));
+
+    Kokkos::TeamPolicy<DeviceType> auto_policy(
+        ceil(1. * _D.extent(0) / _N_team), Kokkos::AUTO(), Kokkos::AUTO());
+    Kokkos::TeamPolicy<DeviceType> tuned_policy(
+        ceil(1. * _D.extent(0) / _N_team), _team_size, _vector_length);
+    Kokkos::TeamPolicy<DeviceType> policy;
+
+    if (_team_size < 1)
+      policy = auto_policy;
+    else
+      policy = tuned_policy;
+
+    size_t bytes_0 = ValuesViewType::shmem_size(_N_team, 5);
+    policy.set_scratch_size(0, Kokkos::PerTeam(bytes_0));
+
+    timer.reset();
+    Kokkos::parallel_for(name.c_str(), policy, *this);
+    exec_space().fence();
+    double sec = timer.seconds();
+    Kokkos::Profiling::popRegion();
+
+    return sec;
+  }
+};
---
+++ perf_test/batched/sparse/KokkosBatched_Test_Sparse_Helper.hpp
@@ -0,0 +1,294 @@
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 3.4
+//       Copyright (2021) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY NTESS "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL NTESS OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact Siva Rajamanickam (srajama@sandia.gov)
+//
+// ************************************************************************
+//@HEADER
+
+template <class XType>
+void writeArrayToMM(std::string name, const XType x) {
+  std::ofstream myfile;
+  myfile.open(name);
+
+  typename XType::HostMirror x_h = Kokkos::create_mirror_view(x);
+
+  Kokkos::deep_copy(x_h, x);
+
+  myfile << "%% MatrixMarket 2D Array\n%" << std::endl;
+  myfile << x_h.extent(0) << " " << x_h.extent(1) << std::endl;
+
+  for (size_t i = 0; i < x_h.extent(0); ++i) {
+    for (size_t j = 0; j < x_h.extent(1); ++j) {
+      myfile << std::setprecision(15) << x_h(i, j) << " ";
+    }
+    myfile << std::endl;
+  }
+
+  myfile.close();
+}
+
+void readSizesFromMM(std::string name, int &nrows, int &ncols, int &nnz,
+                     int &N) {
+  std::ifstream input(name);
+  while (input.peek() == '%')
+    input.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+  std::string line_sizes;
+
+  getline(input, line_sizes);
+
+  std::stringstream iss(line_sizes);
+
+  int number;
+  std::vector<int> sizes;
+  while (iss >> number) sizes.push_back(number);
+
+  nrows = sizes[0];
+  ncols = sizes[1];
+
+  nnz = 0;
+  N   = 0;
+
+  if (sizes.size() >= 3) nnz = sizes[2];
+
+  if (sizes.size() == 4) N = sizes[3];
+}
+
+template <class XType>
+void readArrayFromMM(std::string name, const XType &x) {
+  std::ifstream input(name);
+
+  while (input.peek() == '%')
+    input.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  input.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+  typename XType::HostMirror x_h = Kokkos::create_mirror_view(x);
+
+  for (size_t i = 0; i < x_h.extent(0); ++i)
+    for (size_t j = 0; j < x_h.extent(1); ++j) input >> x_h(i, j);
+
+  input.close();
+
+  Kokkos::deep_copy(x, x_h);
+
+  /*
+    std::ofstream myfile;
+    myfile.open("x-data.txt");
+
+
+    for (size_t i = 0; i < x_h.extent(0); ++i) {
+      for (size_t j = 0; j < x_h.extent(1); ++j) {
+        myfile << std::setprecision (15) << x_h(i, j) << " ";
+      }
+      myfile << std::endl;
+    }
+
+    myfile.close();
+    */
+}
+
+template <class AType>
+void readDenseFromMM(std::string name, const AType &A) {
+  std::ifstream input(name);
+
+  while (input.peek() == '%')
+    input.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  input.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+  typename AType::HostMirror A_h = Kokkos::create_mirror_view(A);
+
+  Kokkos::deep_copy(A_h, 0.);
+
+  int read_row;
+  int read_col;
+
+  int N, Blk, nnz, nrows;
+  readSizesFromMM(name, Blk, nrows, nnz, N);
+
+  for (int i = 0; i < nnz; ++i) {
+    input >> read_row >> read_col;
+    --read_row;
+    --read_col;
+
+    for (int j = 0; j < N; ++j) input >> A_h(j, read_row, read_col);
+  }
+
+  input.close();
+
+  Kokkos::deep_copy(A, A_h);
+}
+
+template <class VType, class IntType>
+void readCRSFromMM(std::string name, const VType &V, const IntType &r,
+                   const IntType &c) {
+  std::ifstream input(name);
+
+  while (input.peek() == '%')
+    input.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  input.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+  typename VType::HostMirror V_h   = Kokkos::create_mirror_view(V);
+  typename IntType::HostMirror r_h = Kokkos::create_mirror_view(r);
+  typename IntType::HostMirror c_h = Kokkos::create_mirror_view(c);
+
+  int current_row = 0;
+  int read_row;
+
+  size_t nnz = c_h.extent(0);
+  int nrows  = r_h.extent(0) - 1;
+
+  r_h(0) = 0;
+
+  for (size_t i = 0; i < nnz; ++i) {
+    input >> read_row >> c_h(i);
+    --read_row;
+    --c_h(i);
+    for (int tmp_row = current_row + 1; tmp_row <= read_row; ++tmp_row)
+      r_h(tmp_row) = i;
+    current_row = read_row;
+
+    // if (VType::Rank == 1)
+    //  input >> V_h(i);
+    if (VType::Rank == 2)
+      for (size_t j = 0; j < V_h.extent(0); ++j) input >> V_h(j, i);
+  }
+
+  r_h(nrows) = nnz;
+
+  input.close();
+
+  Kokkos::deep_copy(V, V_h);
+  Kokkos::deep_copy(r, r_h);
+  Kokkos::deep_copy(c, c_h);
+
+  /*
+    std::ofstream myfile;
+    myfile.open("a-data.txt");
+
+
+    for (size_t i = 0; i < nrows; ++i) {
+      for (size_t j = r_h(i); j < r_h(i+1); ++j) {
+        myfile << std::setprecision (15) << i+1 << " " << c_h(j)+1 << " " <<
+    V_h(0, j) << std::endl;
+      }
+    }
+
+    myfile.close();
+    */
+}
+
+template <class VType, class IntType>
+void getInvDiagFromCRS(const VType &V, const IntType &r, const IntType &c,
+                       const VType &diag) {
+  auto diag_values_host = Kokkos::create_mirror_view(diag);
+  auto values_host      = Kokkos::create_mirror_view(V);
+  auto row_ptr_host     = Kokkos::create_mirror_view(r);
+  auto colIndices_host  = Kokkos::create_mirror_view(c);
+
+  Kokkos::deep_copy(values_host, V);
+  Kokkos::deep_copy(row_ptr_host, r);
+  Kokkos::deep_copy(colIndices_host, c);
+
+  int current_index;
+  int N       = diag.extent(0);
+  int BlkSize = diag.extent(1);
+
+  for (int i = 0; i < BlkSize; ++i) {
+    for (current_index = row_ptr_host(i); current_index < row_ptr_host(i + 1);
+         ++current_index) {
+      if (colIndices_host(current_index) == i) break;
+    }
+    for (int j = 0; j < N; ++j)
+      diag_values_host(j, i) = 1. / values_host(j, current_index);
+  }
+
+  Kokkos::deep_copy(diag, diag_values_host);
+
+  /*
+    std::ofstream myfile;
+    myfile.open("a-diag.txt");
+
+
+    for (size_t i = 0; i < BlkSize; ++i) {
+      myfile << std::setprecision (15) << i+1 << " " << diag_values_host(0, i)
+    << std::endl;
+    }
+
+    myfile.close();
+    */
+}
+
+template <class execution_space>
+int launch_parameters(int numRows, int nnz, int rows_per_thread, int &team_size,
+                      int vector_length) {
+  int rows_per_team;
+  int nnz_per_row = nnz / numRows;
+  if (nnz_per_row < 1) nnz_per_row = 1;
+
+  // Determine rows per thread
+  if (rows_per_thread < 1) {
+#ifdef KOKKOS_ENABLE_CUDA
+    if (std::is_same<Kokkos::Cuda, execution_space>::value)
+      rows_per_thread = 1;
+    else
+#endif
+    {
+      if (nnz_per_row < 20 && nnz > 5000000) {
+        rows_per_thread = 256;
+      } else
+        rows_per_thread = 64;
+    }
+  }
+
+#ifdef KOKKOS_ENABLE_CUDA
+  if (team_size < 1) team_size = 256 / vector_length;
+#endif
+
+  rows_per_team = rows_per_thread * team_size;
+
+  if (rows_per_team < 0) {
+    int nnz_per_team = 4096;
+    int conc         = execution_space::concurrency();
+    while ((conc * nnz_per_team * 4 > nnz) && (nnz_per_team > 256))
+      nnz_per_team /= 2;
+    int tmp_nnz_per_row = nnz / numRows;
+    rows_per_team = (nnz_per_team + tmp_nnz_per_row - 1) / tmp_nnz_per_row;
+  }
+
+  return rows_per_team;
+}
