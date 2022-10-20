47f802fda6fd5cf058c33e83dc385443b04fe924
James Foucar <jgfouca@sandia.gov>
2022-10-20 14:04:37 -0600

    Fix spadd perf tests

--- perf_test/sparse/KokkosSparse_spadd.cpp
+++ perf_test/sparse/KokkosSparse_spadd.cpp
@@ -200,10 +200,8 @@ void run_experiment(const Params& params) {
   double numericTime  = 0;
 
   // Do an untimed warm up symbolic, and preallocate space for C entries/values
-  spadd_symbolic<KernelHandle, const_lno_view_t, const_lno_nnz_view_t,
-                 const_lno_view_t, const_lno_nnz_view_t, lno_view_t,
-                 lno_nnz_view_t>(&kh, A.graph.row_map, A.graph.entries,
-                                 B.graph.row_map, B.graph.entries, row_mapC);
+  spadd_symbolic(&kh, A.graph.row_map, A.graph.entries,
+                 B.graph.row_map, B.graph.entries, row_mapC);
 
   bool use_kk = !params.use_cusparse && !params.use_mkl;
 
@@ -261,11 +259,9 @@ void run_experiment(const Params& params) {
   for (int sumRep = 0; sumRep < params.repeat; sumRep++) {
     timer.reset();
     if (use_kk) {
-      spadd_symbolic<KernelHandle, const_lno_view_t, const_lno_nnz_view_t,
-                     const_lno_view_t, const_lno_nnz_view_t, lno_view_t,
-                     lno_nnz_view_t>(&kh, A.graph.row_map, A.graph.entries,
-                                     B.graph.row_map, B.graph.entries,
-                                     row_mapC);
+      spadd_symbolic(&kh, A.graph.row_map, A.graph.entries,
+                     B.graph.row_map, B.graph.entries,
+                     row_mapC);
       c_nnz = addHandle->get_c_nnz();
     } else if (params.use_cusparse) {
 #ifdef KOKKOSKERNELS_ENABLE_TPL_CUSPARSE
