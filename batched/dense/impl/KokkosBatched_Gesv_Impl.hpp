788018fd43784fd8020e01a18dde0fe1bf7323c1
Luc Berger-Vergiat <lberge@sandia.gov>
2023-04-06 11:29:11 -0600

    Batched Gesv: initializing variable to make compiler happy
    Explicitely intializing the reducer type avoids warning in the
    nightly GCC 910 build.

--- batched/dense/impl/KokkosBatched_Gesv_Impl.hpp
+++ batched/dense/impl/KokkosBatched_Gesv_Impl.hpp
@@ -291,6 +291,8 @@ KOKKOS_INLINE_FUNCTION int TeamVectorStaticPivoting<MemberType>::invoke(
   for (size_t i = 0; i < n; ++i) {
     int row_index, col_index;
     reducer_value_type value;
+    value.val = Kokkos::reduction_identity<value_type>::max();
+    value.loc = Kokkos::reduction_identity<int>::min();
     Kokkos::MaxLoc<value_type, int> reducer_value(value);
     Kokkos::parallel_reduce(
         Kokkos::TeamVectorRange(member, n),
