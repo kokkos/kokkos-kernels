e00e2123b350d12ebafb37ef4fe60ad14c2adc5c
James Foucar <jgfouca@sandia.gov>
2023-04-14 14:23:32 -0600

    Working

--- perf_test/sparse/KokkosSparse_par_ilut.cpp
+++ perf_test/sparse/KokkosSparse_par_ilut.cpp
@@ -45,6 +45,10 @@ namespace {
 using KokkosSparse::Experimental::par_ilut_symbolic;
 using KokkosSparse::Experimental::par_ilut_numeric;
 
+using KokkosSparse::Experimental::spiluk_symbolic;
+using KokkosSparse::Experimental::spiluk_numeric;
+using KokkosSparse::Experimental::SPILUKAlgorithm;
+
 // Build up useful types
 using scalar_t  = default_scalar;
 using lno_t     = default_lno_t;
@@ -64,7 +68,8 @@ using KernelHandle = KokkosKernels::Experimental::KokkosKernelsHandle<
 using float_t = typename Kokkos::ArithTraits<scalar_t>::mag_type;
 
 ///////////////////////////////////////////////////////////////////////////////
-void run_par_ilut_test(KernelHandle& kh, const sp_matrix_type& A, int rows, int team_size, int loop)
+int run_par_ilut_test(KernelHandle& kh, const sp_matrix_type& A,
+                      const int rows, const int team_size, const int loop)
 ///////////////////////////////////////////////////////////////////////////////
 {
   auto par_ilut_handle = kh.get_par_ilut_handle();
@@ -79,22 +84,25 @@ void run_par_ilut_test(KernelHandle& kh, const sp_matrix_type& A, int rows, int 
   RowMapType U_row_map("U_row_map", rows + 1);
 
   // Initial L/U approximations for A
-  EntriesType L_entries("L_entries");
-  ValuesType L_values("L_values");
-  EntriesType U_entries("U_entries");
-  ValuesType U_values("U_values");
+  EntriesType L_entries("L_entries", 0);
+  ValuesType L_values("L_values", 0);
+  EntriesType U_entries("U_entries", 0);
+  ValuesType U_values("U_values", 0);
 
   Kokkos::Timer timer;
   double min_time = std::numeric_limits<double>::infinity();
   double max_time = 0.0;
   double ave_time = 0.0;
+  int num_iters = -1;
+  size_type nnzL = 0;
+  size_type nnzU = 0;
   for (int i = 0; i < loop; ++i) {
     // Run par_ilut
     timer.reset();
     par_ilut_symbolic(&kh, A_row_map, A_entries, L_row_map, U_row_map);
 
-    const size_type nnzL = par_ilut_handle->get_nnzL();
-    const size_type nnzU = par_ilut_handle->get_nnzU();
+    nnzL = par_ilut_handle->get_nnzL();
+    nnzU = par_ilut_handle->get_nnzU();
 
     Kokkos::resize(L_entries, nnzL);
     Kokkos::resize(U_entries, nnzU);
@@ -110,7 +118,7 @@ void run_par_ilut_test(KernelHandle& kh, const sp_matrix_type& A, int rows, int 
     Kokkos::fence();
 
     // Check worked
-    const auto num_iters = par_ilut_handle->get_num_iters();
+    num_iters = par_ilut_handle->get_num_iters();
     KK_REQUIRE_MSG(num_iters < par_ilut_handle->get_max_iter(), "par_ilut hit max iters");
 
     // Measure time
@@ -122,16 +130,22 @@ void run_par_ilut_test(KernelHandle& kh, const sp_matrix_type& A, int rows, int 
     // Reset inputs
     Kokkos::deep_copy(L_row_map, 0);
     Kokkos::deep_copy(U_row_map, 0);
+
+    // Report run so user knows something is happening
+    std::cout << "PAR_ILUT Finished a run in:  " << time << " seconds and " << num_iters << " iters" << std::endl;
   }
 
   std::cout << "PAR_ILUT LOOP_AVG_TIME:  " << ave_time / loop << std::endl;
   std::cout << "PAR_ILUT LOOP_MAX_TIME:  " << max_time << std::endl;
   std::cout << "PAR_ILUT LOOP_MIN_TIME:  " << min_time << std::endl;
+
+  return num_iters;
 }
 
 #ifdef USE_GINKGO
 ///////////////////////////////////////////////////////////////////////////////
-void run_par_ilut_test_ginkgo(KernelHandle& kh, const sp_matrix_type& A, int rows, int team_size, int loop)
+void run_par_ilut_test_ginkgo(KernelHandle& kh, const sp_matrix_type& A,
+                              const int rows, const int team_size, const int loop, const int num_iters)
 ///////////////////////////////////////////////////////////////////////////////
 {
   auto par_ilut_handle = kh.get_par_ilut_handle();
@@ -169,12 +183,16 @@ void run_par_ilut_test_ginkgo(KernelHandle& kh, const sp_matrix_type& A, int row
     auto fact = gko::factorization::ParIlut<scalar_t, lno_t>::build()
       .with_fill_in_limit(par_ilut_handle->get_fill_in_limit())
       .with_approximate_select(false)
+      .with_iterations(num_iters)
       .on(exec)->generate(a_mtx);
 
     double time = timer.seconds();
     ave_time += time;
     if (time > max_time) max_time = time;
     if (time < min_time) min_time = time;
+
+    // Report run so user knows something is happening
+    std::cout << "GINKGO Finished a run in:  " << time << " seconds" << std::endl;
   }
 
   std::cout << "GINKGO LOOP_AVG_TIME:  " << ave_time / loop << std::endl;
@@ -184,13 +202,95 @@ void run_par_ilut_test_ginkgo(KernelHandle& kh, const sp_matrix_type& A, int row
 #endif
 
 ///////////////////////////////////////////////////////////////////////////////
-int test_par_ilut_perf(int rows, int nnz_per_row, float bandwidth_per_nnz,
-                       int team_size, int loop)
+void run_spiluk_test(KernelHandle& kh, const sp_matrix_type& A,
+                     const int rows, const int team_size, const int loop)
 ///////////////////////////////////////////////////////////////////////////////
 {
-  // Generate A
-  std::cout << "Testing " << std::endl;
+  constexpr int EXPAND_FACT = 10;
+  const lno_t fill_lev = 2;
+  const size_type handle_nnz = EXPAND_FACT * A.nnz() * (fill_lev + 1);
+  kh.create_spiluk_handle(SPILUKAlgorithm::SEQLVLSCHD_TP1, rows, handle_nnz, handle_nnz);
+  auto spiluk_handle = kh.get_spiluk_handle();
 
+  // Pull out views from CRS
+  auto A_row_map = A.graph.row_map;
+  auto A_entries = A.graph.entries;
+  auto A_values  = A.values;
+
+  // Allocate L and U CRS views as outputs
+  RowMapType L_row_map("L_row_map", rows + 1);
+  RowMapType U_row_map("U_row_map", rows + 1);
+
+  // Initial L/U approximations for A
+  EntriesType L_entries("L_entries", handle_nnz);
+  ValuesType L_values("L_values", handle_nnz);
+  EntriesType U_entries("U_entries", handle_nnz);
+  ValuesType U_values("U_values", handle_nnz);
+
+  Kokkos::Timer timer;
+  double min_time = std::numeric_limits<double>::infinity();
+  double max_time = 0.0;
+  double ave_time = 0.0;
+  for (int i = 0; i < loop; ++i) {
+    // Run par_ilut
+    timer.reset();
+    spiluk_symbolic(&kh, fill_lev, A_row_map, A_entries, L_row_map, L_entries, U_row_map, U_entries);
+
+    const size_type nnzL = spiluk_handle->get_nnzL();
+    const size_type nnzU = spiluk_handle->get_nnzU();
+
+    Kokkos::resize(L_entries, nnzL);
+    Kokkos::resize(U_entries, nnzU);
+    Kokkos::resize(L_values, nnzL);
+    Kokkos::resize(U_values, nnzU);
+
+    spiluk_numeric(&kh, fill_lev, A_row_map, A_entries, A_values, L_row_map, L_entries,
+                   L_values, U_row_map, U_entries, U_values);
+    Kokkos::fence();
+
+    // Measure time
+    double time = timer.seconds();
+    ave_time += time;
+    if (time > max_time) max_time = time;
+    if (time < min_time) min_time = time;
+
+    // Reset inputs
+    Kokkos::deep_copy(L_row_map, 0);
+    Kokkos::deep_copy(U_row_map, 0);
+    Kokkos::deep_copy(L_entries, 0);
+    Kokkos::deep_copy(U_entries, 0);
+    Kokkos::deep_copy(L_values, 0);
+    Kokkos::deep_copy(U_values, 0);
+    Kokkos::resize(L_entries, handle_nnz);
+    Kokkos::resize(U_entries, handle_nnz);
+
+    spiluk_handle->reset_handle(rows, handle_nnz, handle_nnz);
+
+    // Report run so user knows something is happening
+    std::cout << "SPILUK Finished a run in:  " << time << " seconds" << std::endl;
+  }
+
+  std::cout << "SPILUK LOOP_AVG_TIME:  " << ave_time / loop << std::endl;
+  std::cout << "SPILUK LOOP_MAX_TIME:  " << max_time << std::endl;
+  std::cout << "SPILUK LOOP_MIN_TIME:  " << min_time << std::endl;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+int test_par_ilut_perf(const int rows, const int nnz_per_row, const int bandwidth_per_nnz,
+                       const int team_size, const int loop)
+///////////////////////////////////////////////////////////////////////////////
+{
+  KernelHandle kh;
+  kh.create_par_ilut_handle();
+
+  // Report test config to user
+  auto par_ilut_handle = kh.get_par_ilut_handle();
+  par_ilut_handle->set_team_size(team_size);
+  par_ilut_handle->set_nrows(rows);
+
+  const auto default_policy = par_ilut_handle->get_default_team_policy();
+
+  // Generate A
   size_type nnz   = rows * nnz_per_row;
   const lno_t bandwidth = nnz_per_row * bandwidth_per_nnz;
   const lno_t row_size_variance = 0;
@@ -200,14 +300,24 @@ int test_par_ilut_perf(int rows, int nnz_per_row, float bandwidth_per_nnz,
 
   KokkosSparse::sort_crs_matrix(A);
 
-  KernelHandle kh;
-  kh.create_par_ilut_handle();
+  // Report test config
+  std::cout << "Testing par_ilut with rows=" << rows
+            << "\n  nnz_per_row=" << nnz_per_row
+            << "\n  bandwidth_per_nnz=" << bandwidth_per_nnz
+            << "\n  total nnz=" << A.nnz()
+            << "\n  league_size=" << default_policy.league_size()
+            << "\n  team_size=" << default_policy.team_size()
+            << "\n  concurrent teams=" << exe_space::concurrency() / default_policy.team_size()
+            << "\n  loop=" << loop
+            << std::endl;
 
-  run_par_ilut_test(kh, A, rows, team_size, loop);
+  const auto num_iters = run_par_ilut_test(kh, A, rows, team_size, loop);
 
 #ifdef USE_GINKGO
-  run_par_ilut_test_ginkgo(kh, A, rows, team_size, loop);
+  run_par_ilut_test_ginkgo(kh, A, rows, team_size, loop, num_iters);
 #endif
+
+  run_spiluk_test(kh, A, rows, team_size, loop);
 
   return 0;
 }
@@ -225,7 +335,7 @@ void print_help_par_ilut()
   printf("  -n [N]  : generate a semi-random banded NxN matrix. Default 10000.\n");
   printf("  -z [Z]  : number nnz per row. Default is 1% of N.\n");
   printf("  -b [B]  : bandwidth nnz multiplier. Default is 5.\n");
-  printf("  -ts [T] : Number of threads per team.\n");
+  printf("  -ts [T] : Number of threads per team. Default is 1 on OpenMP, nnz_per_row on CUDA\n");
   //printf("  -vl [V] : Vector-length (i.e. how many Cuda threads are a Kokkos 'thread').\n");
   printf("  -l [L]  : How many runs to aggregate average time. Default is 10\n\n");
 }
@@ -268,6 +378,7 @@ int main(int argc, char **argv)
     return 0;
   }
 
+  // Handle user options
   for (int i = 1; i < argc; i++) {
     if ((strcmp(argv[i], "--help") == 0) || (strcmp(argv[i], "-h") == 0)) {
       print_help_par_ilut();
@@ -276,6 +387,18 @@ int main(int argc, char **argv)
     else {
       handle_int_arg(argc, argv, i, option_map);
     }
+  }
+
+  if (rows < 100) {
+    throw std::runtime_error("Need to have at least 100 rows");
+  }
+
+  // Set dependent defaults
+  if (nnz_per_row == -1) {
+    nnz_per_row = rows / 100;
+  }
+  if (team_size == -1) {
+    team_size = KokkosKernels::Impl::kk_is_gpu_exec_space<exe_space>() ? nnz_per_row : 1;
   }
 
   Kokkos::initialize(argc, argv);
