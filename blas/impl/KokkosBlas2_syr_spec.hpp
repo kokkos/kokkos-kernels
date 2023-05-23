c2a10b91a56b59af8f8548988eff73890bb390cb
Ernesto Prudencio <eeprude@sandia.gov>
2023-05-22 20:06:25 -0600

    Sync with develop

--- blas/impl/KokkosBlas2_syr_spec.hpp
+++ blas/impl/KokkosBlas2_syr_spec.hpp
@@ -133,6 +133,5 @@ struct SYR {
       false, true>;
 
 #include <KokkosBlas2_syr_tpl_spec_decl.hpp>
-#include <generated_specializations_hpp/KokkosBlas2_syr_eti_spec_decl.hpp>
 
 #endif  // KOKKOSBLAS2_SYR_SPEC_HPP_
