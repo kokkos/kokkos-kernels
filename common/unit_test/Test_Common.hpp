10d44dd7e5f02fb3fa6b15a457ee3098aebbc81b
Carl Pearson <cwpears@sandia.gov>
2023-07-20 13:27:51 -0600

    Test_Common.hpp: remove duplicate LowerBound and UpperBound

--- common/unit_test/Test_Common.hpp
+++ common/unit_test/Test_Common.hpp
@@ -17,8 +17,6 @@
 #define TEST_COMMON_HPP
 
 #include <Test_Common_ArithTraits.hpp>
-#include <Test_Common_LowerBound.hpp>
-#include <Test_Common_UpperBound.hpp>
 // #include<Test_Common_float128.hpp>
 #include <Test_Common_set_bit_count.hpp>
 #include <Test_Common_Sorting.hpp>
