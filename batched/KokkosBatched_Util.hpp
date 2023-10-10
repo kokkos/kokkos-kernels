3223af39c87db07b89959bde8b4491939da89d48
Ernesto Prudencio <eeprude@sandia.gov>
2023-10-10 11:23:11 -0600

    Backup

--- batched/KokkosBatched_Util.hpp
+++ batched/KokkosBatched_Util.hpp
@@ -21,6 +21,7 @@
 // no experimental name space guard for trilinos
 #define __KOKKOSBATCHED_PROMOTION__ 1
 
+#include <iostream>
 #include <iomanip>
 #include <random>
 #include <string>
--- batched/dense/impl/KokkosBatched_HostLevel_Gemm_Serial_Impl.hpp
+++ batched/dense/impl/KokkosBatched_HostLevel_Gemm_Serial_Impl.hpp
@@ -16,6 +16,7 @@
 #ifndef __KOKKOSBATCHED_HOSTLEVEL_GEMM_SERIAL_IMPL_HPP__
 #define __KOKKOSBATCHED_HOSTLEVEL_GEMM_SERIAL_IMPL_HPP__
 #include "KokkosBatched_Gemm_Decl.hpp"
+#include <iostream>
 
 namespace KokkosBatched {
 namespace Impl {
