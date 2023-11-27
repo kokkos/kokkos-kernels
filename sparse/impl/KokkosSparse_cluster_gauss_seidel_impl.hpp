89d149e8d18f4369a943c0d26eed03dba06cee53
Ernesto Prudencio <eeprude@sandia.gov>
2023-11-27 14:09:36 -0700

    Removing 'ClusteringAlgorithm::'

--- sparse/impl/KokkosSparse_cluster_gauss_seidel_impl.hpp
+++ sparse/impl/KokkosSparse_cluster_gauss_seidel_impl.hpp
@@ -555,23 +555,22 @@ class ClusterGaussSeidel {
     }
     nnz_view_t vertClusters;
     auto clusterAlgo = gsHandle->get_clustering_algo();
-    if (clusterAlgo == ClusteringAlgorithm::CLUSTER_DEFAULT)
-      clusterAlgo = ClusteringAlgorithm::CLUSTER_MIS2;
+    if (clusterAlgo == CLUSTER_DEFAULT) clusterAlgo = CLUSTER_MIS2;
     switch (clusterAlgo) {
-      case ClusteringAlgorithm::CLUSTER_MIS2: {
+      case CLUSTER_MIS2: {
         vertClusters =
             KokkosGraph::graph_mis2_aggregate<MyExecSpace, raw_rowmap_t,
                                               raw_colinds_t, nnz_view_t>(
                 raw_sym_xadj, raw_sym_adj, numClusters);
         break;
       }
-      case ClusteringAlgorithm::CLUSTER_BALLOON: {
+      case CLUSTER_BALLOON: {
         BalloonClustering<HandleType, raw_rowmap_t, raw_colinds_t> balloon(
             num_rows, raw_sym_xadj, raw_sym_adj);
         vertClusters = balloon.run(clusterSize);
         break;
       }
-      case ClusteringAlgorithm::CLUSTER_DEFAULT: {
+      case CLUSTER_DEFAULT: {
         throw std::logic_error(
             "Logic to choose default clustering algorithm is incorrect");
       }
