2418fd9b17ca48924b4d58cc877bdff3a4a496d2
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2024-08-28 04:31:32 +0900

    format

--- batched/dense/impl/KokkosBatched_Pttrs_Serial_Internal.hpp
+++ batched/dense/impl/KokkosBatched_Pttrs_Serial_Internal.hpp
@@ -25,16 +25,14 @@ namespace KokkosBatched {
 template <typename ArgUplo, typename AlgoType>
 struct SerialPttrsInternal {
   template <typename ValueType>
-  KOKKOS_INLINE_FUNCTION static int invoke(
-      const int n, const ValueType *KOKKOS_RESTRICT d, const int ds0,
-      const ValueType *KOKKOS_RESTRICT e, const int es0,
-      ValueType *KOKKOS_RESTRICT b, const int bs0);
+  KOKKOS_INLINE_FUNCTION static int invoke(const int n, const ValueType *KOKKOS_RESTRICT d, const int ds0,
+                                           const ValueType *KOKKOS_RESTRICT e, const int es0,
+                                           ValueType *KOKKOS_RESTRICT b, const int bs0);
 
   template <typename ValueType>
-  KOKKOS_INLINE_FUNCTION static int invoke(
-      const int n, const ValueType *KOKKOS_RESTRICT d, const int ds0,
-      const Kokkos::complex<ValueType> *KOKKOS_RESTRICT e, const int es0,
-      Kokkos::complex<ValueType> *KOKKOS_RESTRICT b, const int bs0);
+  KOKKOS_INLINE_FUNCTION static int invoke(const int n, const ValueType *KOKKOS_RESTRICT d, const int ds0,
+                                           const Kokkos::complex<ValueType> *KOKKOS_RESTRICT e, const int es0,
+                                           Kokkos::complex<ValueType> *KOKKOS_RESTRICT b, const int bs0);
 };
 
 ///
@@ -44,8 +42,7 @@ struct SerialPttrsInternal {
 template <typename ArgUplo, typename AlgoType>
 template <typename ValueType>
 KOKKOS_INLINE_FUNCTION int SerialPttrsInternal<ArgUplo, AlgoType>::invoke(
-    const int n, const ValueType *KOKKOS_RESTRICT d, const int ds0,
-    const ValueType *KOKKOS_RESTRICT e, const int es0,
+    const int n, const ValueType *KOKKOS_RESTRICT d, const int ds0, const ValueType *KOKKOS_RESTRICT e, const int es0,
     ValueType *KOKKOS_RESTRICT b, const int bs0) {
   // Solve A * X = B using the factorization L * D * L**T
   for (int i = 1; i < n; i++) {
@@ -68,50 +65,23 @@ KOKKOS_INLINE_FUNCTION int SerialPttrsInternal<ArgUplo, AlgoType>::invoke(
 template <typename ArgUplo, typename AlgoType>
 template <typename ValueType>
 KOKKOS_INLINE_FUNCTION int SerialPttrsInternal<ArgUplo, AlgoType>::invoke(
-    const int n, const ValueType *KOKKOS_RESTRICT d, const int ds0,
-    const Kokkos::complex<ValueType> *KOKKOS_RESTRICT e, const int es0,
-    Kokkos::complex<ValueType> *KOKKOS_RESTRICT b, const int bs0) {
+    const int n, const ValueType *KOKKOS_RESTRICT d, const int ds0, const Kokkos::complex<ValueType> *KOKKOS_RESTRICT e,
+    const int es0, Kokkos::complex<ValueType> *KOKKOS_RESTRICT b, const int bs0) {
   // Solve A * X = B using the factorization L * D * L**H
   for (int i = 1; i < n; i++) {
-    auto tmp_e = std::is_same_v<ArgUplo, Uplo::Upper>
-                     ? Kokkos::conj(e[(i - 1) * es0])
-                     : e[(i - 1) * es0];
+    auto tmp_e = std::is_same_v<ArgUplo, Uplo::Upper> ? Kokkos::conj(e[(i - 1) * es0]) : e[(i - 1) * es0];
     b[i * bs0] -= tmp_e * b[(i - 1) * bs0];
   }
 
   b[(n - 1) * bs0] /= d[(n - 1) * ds0];
 
   for (int i = n - 2; i >= 0; i--) {
-    auto tmp_e = std::is_same_v<ArgUplo, Uplo::Lower> ? Kokkos::conj(e[i * es0])
-                                                      : e[i * es0];
+    auto tmp_e = std::is_same_v<ArgUplo, Uplo::Lower> ? Kokkos::conj(e[i * es0]) : e[i * es0];
     b[i * bs0] = b[i * bs0] / d[i * ds0] - b[(i + 1) * bs0] * tmp_e;
   }
 
   return 0;
 }
-
-/*
-template <>
-template <typename ValueType>
-KOKKOS_INLINE_FUNCTION int
-SerialPttrsInternal<Uplo::Upper, Algo::Pttrs::Unblocked>::invoke(
-    const int n, const ValueType *KOKKOS_RESTRICT d, const int ds0,
-    const Kokkos::complex<ValueType> *KOKKOS_RESTRICT e, const int es0,
-    Kokkos::complex<ValueType> *KOKKOS_RESTRICT b, const int bs0) {
-  // Solve A * X = B using the factorization A = U**H * D * U
-  for (int i = 1; i < n; i++) {
-    b[i * bs0] -= Kokkos::conj(e[(i - 1) * es0]) * b[(i - 1) * bs0];
-  }
-
-  b[(n - 1) * bs0] /= d[(n - 1) * ds0];
-
-  for (int i = n - 2; i >= 0; i--) {
-    b[i * bs0] = b[i * bs0] / d[i * ds0] - b[(i + 1) * bs0] * e[i * es0];
-  }
-
-  return 0;
-}
-*/
 
 }  // namespace KokkosBatched
 
