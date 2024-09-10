91c405df53ccdcbb89da3c0cddb5fa927c5fef94
yasahi-hpc <57478230+yasahi-hpc@users.noreply.github.com>
2024-09-10 04:03:17 +0200

    implement batched serial pbtrf (#2322)
    * implement batched serial pbtrf
    
    * fix: docstring
    
    * Add tests for info
    
    * fix CodeQL
    
    * fix: type
    
    * Add a analytical test case for pbtrf
    
    ---------
    
    Co-authored-by: Yuuichi Asahi <y.asahi@nr.titech.ac.jp>

--- batched/dense/unit_test/Test_Batched_Dense.hpp
+++ batched/dense/unit_test/Test_Batched_Dense.hpp
@@ -55,6 +55,9 @@
 #include "Test_Batched_SerialPttrs.hpp"
 #include "Test_Batched_SerialPttrs_Real.hpp"
 #include "Test_Batched_SerialPttrs_Complex.hpp"
+#include "Test_Batched_SerialPbtrf.hpp"
+#include "Test_Batched_SerialPbtrf_Real.hpp"
+#include "Test_Batched_SerialPbtrf_Complex.hpp"
 
 // Team Kernels
 #include "Test_Batched_TeamAxpy.hpp"
