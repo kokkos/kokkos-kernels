bd8a07aeb0c372387ec4de7c311af1303deb8acd
Brian Kelley <bmkelle@sandia.gov>
2024-11-05 16:53:14 -0700

    Fix UB sanitizer error in graph coarsening
    Initialize bool member of struct. If uninitialized
    its memory can contain any 8-bit value, not just 0 or 1.
    
    Signed-off-by: Brian Kelley <bmkelle@sandia.gov>

--- graph/src/KokkosGraph_CoarsenConstruct.hpp
+++ graph/src/KokkosGraph_CoarsenConstruct.hpp
@@ -177,8 +177,8 @@ class coarse_builder {
     matrix_t mtx;
     vtx_view_t vtx_wgts;
     matrix_t interp_mtx;
-    int level;
-    bool uniform_weights;
+    int level = 0;
+    bool uniform_weights = false;
   };
 
   // define behavior-controlling enums
