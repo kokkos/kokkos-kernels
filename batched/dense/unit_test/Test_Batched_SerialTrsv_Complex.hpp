87b2be85bdfd80f47341fc8ddd0bea4057d239a0
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2024-12-06 22:57:05 +0900

    fix code QR
    Signed-off-by: Yuuichi Asahi <y.asahi@nr.titech.ac.jp>

--- batched/dense/unit_test/Test_Batched_SerialTrsv_Complex.hpp
+++ batched/dense/unit_test/Test_Batched_SerialTrsv_Complex.hpp
@@ -80,63 +80,63 @@ TEST_F(TestCategory, batched_scalar_serial_trsv_u_c_n_fcomplex_fcomplex) {
 
 #if defined(KOKKOSKERNELS_INST_COMPLEX_DOUBLE)
 TEST_F(TestCategory, batched_scalar_serial_trsv_l_nt_u_dcomplex_dcomplex) {
-  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Lower, Trans::NoTranspose, Diag::Unit> param_tag_type;
+  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Lower, Trans::NoTranspose, Diag::Unit>;
   using algo_tag_type  = Algo::Trsv::Unblocked;
   test_batched_trsv<TestDevice, Kokkos::complex<double>, Kokkos::complex<double>, param_tag_type, algo_tag_type>();
 }
 TEST_F(TestCategory, batched_scalar_serial_trsv_l_nt_n_dcomplex_dcomplex) {
-  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Lower, Trans::NoTranspose, Diag::NonUnit> param_tag_type;
+  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Lower, Trans::NoTranspose, Diag::NonUnit>;
   using algo_tag_type  = Algo::Trsv::Unblocked;
   test_batched_trsv<TestDevice, Kokkos::complex<double>, Kokkos::complex<double>, param_tag_type, algo_tag_type>();
 }
 TEST_F(TestCategory, batched_scalar_serial_trsv_l_t_u_dcomplex_dcomplex) {
-  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Lower, Trans::Transpose, Diag::Unit> param_tag_type;
+  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Lower, Trans::Transpose, Diag::Unit>;
   using algo_tag_type  = Algo::Trsv::Unblocked;
   test_batched_trsv<TestDevice, Kokkos::complex<double>, Kokkos::complex<double>, param_tag_type, algo_tag_type>();
 }
 TEST_F(TestCategory, batched_scalar_serial_trsv_l_t_n_dcomplex_dcomplex) {
-  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Lower, Trans::Transpose, Diag::NonUnit> param_tag_type;
+  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Lower, Trans::Transpose, Diag::NonUnit>;
   using algo_tag_type  = Algo::Trsv::Unblocked;
   test_batched_trsv<TestDevice, Kokkos::complex<double>, Kokkos::complex<double>, param_tag_type, algo_tag_type>();
 }
 TEST_F(TestCategory, batched_scalar_serial_trsv_l_c_u_dcomplex_dcomplex) {
-  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Lower, Trans::ConjTranspose, Diag::Unit> param_tag_type;
+  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Lower, Trans::ConjTranspose, Diag::Unit>;
   using algo_tag_type  = Algo::Trsv::Unblocked;
   test_batched_trsv<TestDevice, Kokkos::complex<double>, Kokkos::complex<double>, param_tag_type, algo_tag_type>();
 }
 TEST_F(TestCategory, batched_scalar_serial_trsv_l_c_n_dcomplex_dcomplex) {
-  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Lower, Trans::ConjTranspose, Diag::NonUnit> param_tag_type;
+  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Lower, Trans::ConjTranspose, Diag::NonUnit>;
   using algo_tag_type  = Algo::Trsv::Unblocked;
   test_batched_trsv<TestDevice, Kokkos::complex<double>, Kokkos::complex<double>, param_tag_type, algo_tag_type>();
 }
 
 TEST_F(TestCategory, batched_scalar_serial_trsv_u_nt_u_dcomplex_dcomplex) {
-  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Upper, Trans::NoTranspose, Diag::Unit> param_tag_type;
+  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Upper, Trans::NoTranspose, Diag::Unit>;
   using algo_tag_type  = Algo::Trsv::Unblocked;
   test_batched_trsv<TestDevice, Kokkos::complex<double>, Kokkos::complex<double>, param_tag_type, algo_tag_type>();
 }
 TEST_F(TestCategory, batched_scalar_serial_trsv_u_nt_n_dcomplex_dcomplex) {
-  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Upper, Trans::NoTranspose, Diag::NonUnit> param_tag_type;
+  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Upper, Trans::NoTranspose, Diag::NonUnit>;
   using algo_tag_type  = Algo::Trsv::Unblocked;
   test_batched_trsv<TestDevice, Kokkos::complex<double>, Kokkos::complex<double>, param_tag_type, algo_tag_type>();
 }
 TEST_F(TestCategory, batched_scalar_serial_trsv_u_t_u_dcomplex_dcomplex) {
-  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Upper, Trans::Transpose, Diag::Unit> param_tag_type;
+  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Upper, Trans::Transpose, Diag::Unit>;
   using algo_tag_type  = Algo::Trsv::Unblocked;
   test_batched_trsv<TestDevice, Kokkos::complex<double>, Kokkos::complex<double>, param_tag_type, algo_tag_type>();
 }
 TEST_F(TestCategory, batched_scalar_serial_trsv_u_t_n_dcomplex_dcomplex) {
-  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Upper, Trans::Transpose, Diag::NonUnit> param_tag_type;
+  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Upper, Trans::Transpose, Diag::NonUnit>;
   using algo_tag_type  = Algo::Trsv::Unblocked;
   test_batched_trsv<TestDevice, Kokkos::complex<double>, Kokkos::complex<double>, param_tag_type, algo_tag_type>();
 }
 TEST_F(TestCategory, batched_scalar_serial_trsv_u_c_u_dcomplex_dcomplex) {
-  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Upper, Trans::ConjTranspose, Diag::Unit> param_tag_type;
+  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Upper, Trans::ConjTranspose, Diag::Unit>;
   using algo_tag_type  = Algo::Trsv::Unblocked;
   test_batched_trsv<TestDevice, Kokkos::complex<double>, Kokkos::complex<double>, param_tag_type, algo_tag_type>();
 }
 TEST_F(TestCategory, batched_scalar_serial_trsv_u_c_n_dcomplex_dcomplex) {
-  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Upper, Trans::ConjTranspose, Diag::NonUnit> param_tag_type;
+  using param_tag_type = ::Test::Trsv::ParamTag<Uplo::Upper, Trans::ConjTranspose, Diag::NonUnit>;
   using algo_tag_type  = Algo::Trsv::Unblocked;
   test_batched_trsv<TestDevice, Kokkos::complex<double>, Kokkos::complex<double>, param_tag_type, algo_tag_type>();
 }
