d8a5ca0576ce99d82649005aca71e2e1587f76b1
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2025-02-04 16:47:32 +0900

    implement batched serial ger
    Signed-off-by: Yuuichi Asahi <y.asahi@nr.titech.ac.jp>

--- batched/dense/unit_test/Test_Batched_Dense.hpp
+++ batched/dense/unit_test/Test_Batched_Dense.hpp
@@ -65,6 +65,7 @@
 #include "Test_Batched_SerialIamax.hpp"
 #include "Test_Batched_SerialGetrf.hpp"
 #include "Test_Batched_SerialGetrs.hpp"
+#include "Test_Batched_SerialGer.hpp"
 
 // Team Kernels
 #include "Test_Batched_TeamAxpy.hpp"
