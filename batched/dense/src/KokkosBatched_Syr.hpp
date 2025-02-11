b48a9b2e498831c40d0de2416f1ecf4c90209a5f
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2025-02-11 14:53:50 +0900

    Add docstring and assertion for ArgUplo and ArgTrans parameters
    Signed-off-by: Yuuichi Asahi <y.asahi@nr.titech.ac.jp>

--- batched/dense/src/KokkosBatched_Syr.hpp
+++ batched/dense/src/KokkosBatched_Syr.hpp
@@ -27,6 +27,11 @@ namespace KokkosBatched {
 ///   A := alpha*x*x**T + A or A := alpha*x*x**H + A
 ///    where alpha is a scalar, x is an n element vector, and A is a n by n symmetric or Hermitian matrix.
 ///
+/// \tparam ArgUplo: Type indicating whether the upper (Uplo::Upper) or lower (Uplo::Lower) triangular part of A is
+/// modified
+/// \tparam ArgTrans: Type indicating whether the transpose (Trans::Transpose) or conjugate transpose
+/// (Trans::ConjTranspose) of x is used
+///
 /// \tparam ScalarType: Input type for the scalar alpha
 /// \tparam XViewType: Input type for the vector x, needs to be a 1D view
 /// \tparam AViewType: Input/output type for the matrix A, needs to be a 2D view
@@ -39,6 +44,11 @@ namespace KokkosBatched {
 ///
 template <typename ArgUplo, typename ArgTrans>
 struct SerialSyr {
+  static_assert(
+      std::is_same_v<ArgUplo, Uplo::Upper> || std::is_same_v<ArgUplo, Uplo::Lower>,
+      "KokkosBatched::syr: Use Uplo::Upper for upper triangular matrix or Uplo::Lower for lower triangular matrix");
+  static_assert(std::is_same_v<ArgTrans, Trans::Transpose> || std::is_same_v<ArgTrans, Trans::ConjTranspose>,
+                "KokkosBatched::syr: Use Trans::Transpose for {s,d,c,z}syr or Trans::ConjTranspose for {c,z}her");
   template <typename ScalarType, typename XViewType, typename AViewType>
   KOKKOS_INLINE_FUNCTION static int invoke(const ScalarType alpha, const XViewType &x, const AViewType &a);
 };
