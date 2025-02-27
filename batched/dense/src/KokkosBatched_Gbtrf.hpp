0d27a28b406ede97c5f530bc43fd7cccd67a818c
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2025-02-27 23:42:46 +0900

    Add docstring and assertion for ArgAlgo parameter in gbtrf
    Signed-off-by: Yuuichi Asahi <y.asahi@nr.titech.ac.jp>

--- batched/dense/src/KokkosBatched_Gbtrf.hpp
+++ batched/dense/src/KokkosBatched_Gbtrf.hpp
@@ -25,6 +25,9 @@ namespace KokkosBatched {
 /// \brief Serial Batched Gbtrf:
 /// Compute a LU factorization of a m-by-n band matrix A using partial
 ///   pivoting with row interchanges.
+/// \tparam ArgAlgo: Type indicating the blocked (KokkosBatched::Algo::Gbtrf::Blocked) or unblocked
+/// (KokkosBatched::Algo::Gbtrf::Unblocked) algorithm to be used
+///
 /// \tparam ABViewType: Input type for the matrix, needs to be a 2D view
 /// \tparam PivViewType: Input type for the pivot indices, needs to be a 1D view
 ///
@@ -32,13 +35,14 @@ namespace KokkosBatched {
 /// \param piv [out]: On exit, the pivot indices, a rank 1 view
 /// \param kl [in]: The number of subdiagonals within the band of A. kl >= 0
 /// \param ku [in]: The number of superdiagonals within the band of A. ku >= 0
-/// \param m [in]: The number of rows of the matrix A. (optional)
+/// \param m [in]: The number of rows of the matrix A. (optional, default is -1, corresponding to m == n)
 ///
 /// No nested parallel_for is used inside of the function.
 ///
 
 template <typename ArgAlgo>
 struct SerialGbtrf {
+  static_assert(std::is_same_v<ArgAlgo, Algo::Gbtrf::Unblocked>, "KokkosBatched::gbtrf: Use Algo::Gbtrf::Unblocked");
   template <typename ABViewType, typename PivViewType>
   KOKKOS_INLINE_FUNCTION static int invoke(const ABViewType &Ab, const PivViewType &piv, const int kl, const int ku,
                                            const int m = -1);
