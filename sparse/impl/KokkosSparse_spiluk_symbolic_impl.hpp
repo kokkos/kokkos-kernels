be301698da7ddf03a4018cbce2933d58e81d7589
Victor Brunini <vebruni@sandia.gov>
2025-09-03 12:45:36 -0600

    spiluk: Limit memory usage for iw work buffer based on input matrix size.
    Observed some application use cases where this buffer was using > 10x the
    amount of memory needed for the matrix being factorized which lead to OOM's
    later in the run because insufficient memory was available.
    
    Signed-off-by: Victor Brunini <vebruni@sandia.gov>

--- sparse/impl/KokkosSparse_spiluk_symbolic_impl.hpp
+++ sparse/impl/KokkosSparse_spiluk_symbolic_impl.hpp
@@ -147,7 +147,10 @@ void level_sched_tp(IlukHandle& thandle, const RowMapType row_map, const Entries
   if (std::is_same<memory_space, Kokkos::CudaSpace>::value) {
     size_t free_byte, total_byte;
     KokkosKernels::Impl::kk_get_free_total_memory<memory_space>(free_byte, total_byte);
-    avail_byte = static_cast<size_t>(0.85 * static_cast<double>(free_byte) / static_cast<double>(nstreams));
+    using nnz_scalar_t = typename IlukHandle::nnz_scalar_t;
+    const double min_buffer_size = 128 * 1024 * 1024; // 128 MB
+    double orig_matrix_bytes = std::max(min_buffer_size, entries.extent(0) * (sizeof(nnz_scalar_t) + sizeof(nnz_lno_t)));
+    avail_byte = static_cast<size_t>(std::min(0.85 * static_cast<double>(free_byte), orig_matrix_bytes) / static_cast<double>(nstreams));
   }
 #endif
 
