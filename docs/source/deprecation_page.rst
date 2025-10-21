0c41a00737da6ff504885d7c42503ee392682b82
Brian Kelley <bmkelle@sandia.gov>
2025-10-21 14:48:31 -0600

    Remove deprecated declarations, macro in sparse
    Signed-off-by: Brian Kelley <bmkelle@sandia.gov>

--- docs/source/deprecation_page.rst
+++ docs/source/deprecation_page.rst
@@ -15,4 +15,3 @@ The following macros are deprecated as they ignore the standard or Kokkos' reser
 - ``__KOKKOSBATCHED_ENABLE_INTEL_MKL__``
 - ``__KOKKOSBATCHED_ENABLE_INTEL_MKL_BATCHED__``
 - ``__KOKKOSBATCHED_ENABLE_AVX__``
-- ``KOKKOS_CUSPARSE_SAFE_CALL``
--- sparse/src/KokkosSparse_StaticCrsGraph.hpp
+++ sparse/src/KokkosSparse_StaticCrsGraph.hpp
@@ -253,12 +253,6 @@ class StaticCrsGraph {
   using staticcrsgraph_type = StaticCrsGraph<DataType, Arg1Type, Arg2Type, Arg3Type, SizeType>;
   using host_mirror_type =
       StaticCrsGraph<data_type, array_layout, typename traits::host_mirror_space, memory_traits, size_type>;
-#ifdef KOKKOS_ENABLE_DEPRECATED_CODE_4
-  [[deprecated(
-      "KokkosSparse::StaticCrsGraph::HostMirror is deprecated please use "
-      "KokkosSparse::StaticCrsGraph::host_mirror_type instead.")]] using HostMirror = host_mirror_type;
-#endif
-
   using row_map_type   = Kokkos::View<const size_type*, array_layout, device_type, memory_traits>;
   using entries_type   = Kokkos::View<data_type*, array_layout, device_type, memory_traits>;
   using row_block_type = Kokkos::View<const size_type*, array_layout, device_type, memory_traits>;
