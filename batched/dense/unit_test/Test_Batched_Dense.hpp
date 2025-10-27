bc28efe5f6216fa0354df9bdef939c0c32ec5580
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2025-10-28 02:15:19 +0900

    support batched axpy of 1D
    Signed-off-by: Yuuichi Asahi <y.asahi@nr.titech.ac.jp>

--- batched/dense/unit_test/Test_Batched_Dense.hpp
+++ batched/dense/unit_test/Test_Batched_Dense.hpp
@@ -5,8 +5,6 @@
 
 // Serial kernels
 #include "Test_Batched_SerialAxpy.hpp"
-#include "Test_Batched_SerialAxpy_Real.hpp"
-#include "Test_Batched_SerialAxpy_Complex.hpp"
 #include "Test_Batched_SerialEigendecomposition.hpp"
 #include "Test_Batched_SerialEigendecomposition_Real.hpp"
 #include "Test_Batched_SerialGesv.hpp"
@@ -53,8 +51,6 @@
 
 // Team Kernels
 #include "Test_Batched_TeamAxpy.hpp"
-#include "Test_Batched_TeamAxpy_Real.hpp"
-#include "Test_Batched_TeamAxpy_Complex.hpp"
 #include "Test_Batched_TeamGesv.hpp"
 #include "Test_Batched_TeamGesv_Real.hpp"
 #include "Test_Batched_TeamInverseLU.hpp"
@@ -75,8 +71,6 @@
 
 // TeamVector Kernels
 #include "Test_Batched_TeamVectorAxpy.hpp"
-#include "Test_Batched_TeamVectorAxpy_Real.hpp"
-#include "Test_Batched_TeamVectorAxpy_Complex.hpp"
 #include "Test_Batched_TeamVectorEigendecomposition.hpp"
 #include "Test_Batched_TeamVectorEigendecomposition_Real.hpp"
 #include "Test_Batched_TeamVectorGesv.hpp"
