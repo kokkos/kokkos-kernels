8831a231f881c5fe40272c59cdd6b910d82826d7
Luc Berger-Vergiat <lberge@sandia.gov>
2026-02-10 12:31:57 -0700

    Fixing format
    Signed-off-by: Luc Berger-Vergiat <lberge@sandia.gov>

--- example/wiki/blas/KokkosBlas1_wiki_axpy.cpp
+++ example/wiki/blas/KokkosBlas1_wiki_axpy.cpp
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 // SPDX-FileCopyrightText: Copyright Contributors to the Kokkos project
 
-#include<Kokkos_Core.hpp>
-#include<KokkosBlas1_axpby.hpp>
+#include <Kokkos_Core.hpp>
+#include <KokkosBlas1_axpby.hpp>
 
 #include <iostream>
 
@@ -10,25 +10,24 @@ int main(int argc, char* argv[]) {
   Kokkos::initialize(argc, argv);
   {
     int N = 10;
-    if(argc > 1) {
+    if (argc > 1) {
       N = atoi(argv[1]);
     }
 
-    Kokkos::View<double*> x("X",N);
-    Kokkos::View<double*> y("Y",N);
-    Kokkos::deep_copy(x,3.0);
-    Kokkos::deep_copy(y,2.0);
+    Kokkos::View<double*> x("X", N);
+    Kokkos::View<double*> y("Y", N);
+    Kokkos::deep_copy(x, 3.0);
+    Kokkos::deep_copy(y, 2.0);
 
     double alpha = 1.5;
 
-    KokkosBlas::axpy(alpha,x,y);
+    KokkosBlas::axpy(alpha, x, y);
 
     double sum = 0.0;
-    Kokkos::parallel_reduce("CheckValue", N, KOKKOS_LAMBDA (const int& i, double& lsum) {
-        lsum += y(i);
-      },sum);
+    Kokkos::parallel_reduce(
+        "CheckValue", N, KOKKOS_LAMBDA(const int& i, double& lsum) { lsum += y(i); }, sum);
 
-    const double expected = N*(2.0+1.5*3.0);
+    const double expected = N * (2.0 + 1.5 * 3.0);
     std::cout << "Sum: " << sum << ", Expected: " << expected << ", Diff: " << sum - expected << std::endl;
   }
 
