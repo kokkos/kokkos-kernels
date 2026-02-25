c5416f40cddbc3a2d3656b4902535aa83dcdf885
James Foucar <jgfouca@sandia.gov>
2026-02-25 13:46:48 -0700

    Formatting
    Signed-off-by: James Foucar <jgfouca@sandia.gov>

--- lapack/src/KokkosLapack_potrf.hpp
+++ lapack/src/KokkosLapack_potrf.hpp
@@ -17,15 +17,11 @@ namespace KokkosLapack {
 /// \tparam AViewType TODO: describe this parameter
 ///
 template <class execution_space, class AViewType>
-void potrf([[maybe_unused]] const execution_space& space,
-           const char uplo[],
-           const int& n,
-           AViewType& A,
+void potrf([[maybe_unused]] const execution_space& space, const char uplo[], const int& n, AViewType& A,
            const int& lda) {
   static_assert(Kokkos::is_execution_space<execution_space>::value,
                 "KokkosLapack::potrf: execution_space must be a valid Kokkos execution space");
-  static_assert(Kokkos::is_view<AViewType>::value,
-                "KokkosLapack::potrf: AViewType must be a Kokkos::View");
+  static_assert(Kokkos::is_view<AViewType>::value, "KokkosLapack::potrf: AViewType must be a Kokkos::View");
 
   // Convert views to unmanaged
   using AViewInternalType = Kokkos::View<typename AViewType::data_type, typename AViewType::array_layout,
@@ -38,10 +34,7 @@ void potrf([[maybe_unused]] const execution_space& space,
 
 // Overload without execution space (uses default)
 template <class AViewType>
-void potrf(const char uplo[],
-                const int& n,
-                AViewType& A,
-                const int& lda) {
+void potrf(const char uplo[], const int& n, AViewType& A, const int& lda) {
   potrf(typename AViewType::execution_space{}, uplo, n, A, lda);
 }
 
