7b586d6a3b031fd0684db45faef12cdd507ff95c
James Foucar <jgfouca@sandia.gov>
2026-03-26 15:29:21 -0600

    Throw error if user tries to use non-existent native potrf impl
    Signed-off-by: James Foucar <jgfouca@sandia.gov>

--- lapack/impl/KokkosLapack_potrf_impl.hpp
+++ lapack/impl/KokkosLapack_potrf_impl.hpp
@@ -13,11 +13,10 @@ namespace Impl {
 // Implementation struct for potrf
 template <class AViewType>
 struct PotrfImpl {
-  // TODO: Add your implementation here
   static void potrf([[maybe_unused]] const char uplo[], [[maybe_unused]] const int& n, [[maybe_unused]] AViewType& A,
                     [[maybe_unused]] const int& lda) {
-    // TODO: Implement your kernel here
-    // Kokkos::abort("KokkosLapack::potrf: Not yet implemented");
+    // TODO: Implement?
+    Kokkos::abort("KokkosLapack::potrf: Not yet implemented. Enable LAPACK or CUSOLVER|ROCSOLVER");
   }
 };
 
