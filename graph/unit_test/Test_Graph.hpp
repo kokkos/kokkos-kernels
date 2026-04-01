9cb0cc3ee68c2c93c765450ea30e2dcde6117569
Luc Berger-Vergiat <lberge@sandia.gov>
2026-04-01 11:43:30 -0600

    Remove KOKKOS_ENABLE_CUDA_LAMBDA checks
    Kokkos always enables cuda lambda when the CUDA backend is enabled
    so we do not need to check for it anymore.
    This was used in batched tests and graph coarsening.
    
    Signed-off-by: Luc Berger-Vergiat <lberge@sandia.gov>

--- graph/unit_test/Test_Graph.hpp
+++ graph/unit_test/Test_Graph.hpp
@@ -7,9 +7,7 @@
 #include "Test_Graph_graph_color_distance2.hpp"
 #include "Test_Graph_graph_color.hpp"
 #include "Test_Graph_mis2.hpp"
-#if !defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_CUDA_LAMBDA)
 #include "Test_Graph_coarsen.hpp"
-#endif
 #include "Test_Graph_rcm.hpp"
 #include "Test_Graph_rcb.hpp"
 
