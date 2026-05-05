a66093533e02b93d8cdb4e62645026cf3b926730
James Foucar <jgfouca@sandia.gov>
2026-05-05 13:05:30 -0600

    Add potrs to check_api_updates
    Signed-off-by: James Foucar <jgfouca@sandia.gov>

--- scripts/check_api_updates.py
+++ scripts/check_api_updates.py
@@ -60,6 +60,7 @@ src_doc_mapping = dict([
                         ('lapack/src/KokkosLapack_geqrf.hpp', ['docs/source/API/lapack/geqrf.rst']),
                         ('lapack/src/KokkosLapack_gemqr.hpp', ['docs/source/API/lapack/gemqr.rst']),
                         ('lapack/src/KokkosLapack_potrf.hpp', ['docs/source/API/lapack/potrf.rst']),
+                        ('lapack/src/KokkosLapack_potrs.hpp', ['docs/source/API/lapack/potrs.rst']),
                         ('lapack/src/KokkosLapack_gesv.hpp', ['docs/source/API/lapack/gesv.rst']),
                         ('lapack/src/KokkosLapack_svd.hpp', ['docs/source/API/lapack/gesvd.rst']),
                         ('lapack/src/KokkosLapack_trtri.hpp', ['docs/source/API/lapack/trtri.rst']),
