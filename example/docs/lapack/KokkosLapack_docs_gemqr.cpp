85ede765d7994885cb95d5fddf5640ff555c52d7
Luc Berger-Vergiat <lberge@sandia.gov>
2026-05-11 17:11:03 -0600

    Applying clang-format
    Signed-off-by: Luc Berger-Vergiat <lberge@sandia.gov>

--- example/docs/lapack/KokkosLapack_docs_gemqr.cpp
+++ example/docs/lapack/KokkosLapack_docs_gemqr.cpp
@@ -10,17 +10,23 @@ int main(void) {
   Kokkos::initialize();
   {
     using execution_space = Kokkos::DefaultExecutionSpace;
-    using KAT = KokkosKernels::ArithTraits<double>;
+    using KAT             = KokkosKernels::ArithTraits<double>;
 
     Kokkos::View<double**, Kokkos::LayoutLeft> A("A", 3, 3), R("R", 3, 3);
-    Kokkos::View<double*, Kokkos::LayoutLeft>  tau("tau", 3);
+    Kokkos::View<double*, Kokkos::LayoutLeft> tau("tau", 3);
     Kokkos::View<int*, Kokkos::LayoutLeft> Info("Info", 1);
 
     auto h_A = Kokkos::create_mirror_view(A);
 
-    h_A(0, 0) = 12; h_A(0, 1) = -51; h_A(0, 2) =   4;
-    h_A(1, 0) =  6; h_A(1, 1) = 167; h_A(1, 2) = -68;
-    h_A(2, 0) = -4; h_A(2, 1) =  24; h_A(2, 2) = -41;
+    h_A(0, 0) = 12;
+    h_A(0, 1) = -51;
+    h_A(0, 2) = 4;
+    h_A(1, 0) = 6;
+    h_A(1, 1) = 167;
+    h_A(1, 2) = -68;
+    h_A(2, 0) = -4;
+    h_A(2, 1) = 24;
+    h_A(2, 2) = -41;
     Kokkos::deep_copy(A, h_A);
     Kokkos::deep_copy(R, A);
 
@@ -33,23 +39,30 @@ int main(void) {
     Kokkos::deep_copy(h_R, R);
 
     Kokkos::View<double**, Kokkos::DefaultHostExecutionSpace> analytic("result", 3, 3);
-    analytic(0, 0) = -14; analytic(0, 1) = -21; analytic(0, 2) = 14;
-    analytic(1, 0) = 0; analytic(1, 1) = -175; analytic(1, 2) = 70;
-    analytic(2, 0) = 0; analytic(2, 1) = 0; analytic(2, 2) = -35;
+    analytic(0, 0) = -14;
+    analytic(0, 1) = -21;
+    analytic(0, 2) = 14;
+    analytic(1, 0) = 0;
+    analytic(1, 1) = -175;
+    analytic(1, 2) = 70;
+    analytic(2, 0) = 0;
+    analytic(2, 1) = 0;
+    analytic(2, 2) = -35;
 
     for (int rowIdx = 0; rowIdx < h_R.extent_int(0); ++rowIdx) {
       for (int colIdx = 0; colIdx < h_R.extent_int(1); ++colIdx) {
-	if (colIdx < rowIdx) {
-	  if (KAT::abs(h_R(rowIdx, colIdx)) > 100*KAT::epsilon()) {
-	    std::cout << "h_R("<< rowIdx << ", " << colIdx << ")=" << h_R(rowIdx, colIdx)
-		      << ", analytic is " << analytic(rowIdx, colIdx) << std::endl;
-	    correct = false;
-	  }
-	} else if (KAT::abs(h_R(rowIdx, colIdx) - analytic(rowIdx, colIdx)) > KAT::abs(analytic(rowIdx, colIdx))*KAT::epsilon()) {
-	  std::cout << "h_R("<< rowIdx << ", " << colIdx << ")=" << h_R(rowIdx, colIdx)
-		    << ", analytic is " << analytic(rowIdx, colIdx) << std::endl;
-	  correct = false;
-	}
+        if (colIdx < rowIdx) {
+          if (KAT::abs(h_R(rowIdx, colIdx)) > 100 * KAT::epsilon()) {
+            std::cout << "h_R(" << rowIdx << ", " << colIdx << ")=" << h_R(rowIdx, colIdx) << ", analytic is "
+                      << analytic(rowIdx, colIdx) << std::endl;
+            correct = false;
+          }
+        } else if (KAT::abs(h_R(rowIdx, colIdx) - analytic(rowIdx, colIdx)) >
+                   KAT::abs(analytic(rowIdx, colIdx)) * KAT::epsilon()) {
+          std::cout << "h_R(" << rowIdx << ", " << colIdx << ")=" << h_R(rowIdx, colIdx) << ", analytic is "
+                    << analytic(rowIdx, colIdx) << std::endl;
+          correct = false;
+        }
       }
     }
   }
