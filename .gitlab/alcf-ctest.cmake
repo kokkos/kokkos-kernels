0ab0af34b804044bafe64263ee9dd3decbbc1df6
Luc Berger <lberge@sandia.gov>
2026-05-18 13:40:49 -0600

    Update .gitlab/alcf-ctest.cmake
    Co-authored-by: yasahi-hpc <57478230+yasahi-hpc@users.noreply.github.com>
    Signed-off-by: Luc Berger <lberge@sandia.gov>

--- .gitlab/alcf-ctest.cmake
+++ .gitlab/alcf-ctest.cmake
@@ -7,17 +7,6 @@ set(CTEST_BUILD_NAME "Linux-g++")
 set(CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
 
 set(CTEST_UPDATE_COMMAND /usr/bin/git)
-set(CTEST_CONFIGURE_COMMAND "cmake -S $ENV{PWD} \
-			    -B kokkos-kernels_build \
-			    -DCMAKE_INSTALL_PREFIX=kokkos-kernels_install \
-			    -DBUILD_SHARED_LIBS=ON \
-			    -DCMAKE_BUILD_TYPE=Release \
-			    -DCMAKE_VERBOSE_MAKEFILE=OFF \
-			    -DKokkos_ROOT=kokkos_install \
-			    -DKokkosKernels_ENABLE_TESTS:BOOL=ON \
-			    -DKokkosKernels_ENABLE_EXAMPLES:BOOL=ON \
-			    -DKokkosKernels_ENABLE_BENCHMARKS:BOOL=ON \
-			    -DKokkosKernels_RUN_BENCHMARKS:BOOL=ON")
 
 set(CTEST_BUILD_COMMAND "cmake --build $ENV{PWD}/kokkos-kernels/build -j 48")
 
