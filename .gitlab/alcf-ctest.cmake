0a03c3ed67580369c6fb918abd8b04b5335edd33
Luc Berger <lberge@sandia.gov>
2026-05-18 13:40:58 -0600

    Update .gitlab/alcf-ctest.cmake
    Co-authored-by: yasahi-hpc <57478230+yasahi-hpc@users.noreply.github.com>
    Signed-off-by: Luc Berger <lberge@sandia.gov>

--- .gitlab/alcf-ctest.cmake
+++ .gitlab/alcf-ctest.cmake
@@ -14,5 +14,72 @@ ctest_start(Nightly)
 ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}")
 ctest_configure(BUILD "${CTEST_BINARY_DIRECTORY}")
 ctest_build(BUILD "${CTEST_BINARY_DIRECTORY}")
-ctest_test(BUILD "${CTEST_BINARY_DIRECTORY}")
+# ------------------------------------------------------------------------------
+# Update source tree
+# ------------------------------------------------------------------------------
+
+ctest_update(
+  SOURCE "${CTEST_SOURCE_DIRECTORY}"
+)
+
+# ------------------------------------------------------------------------------
+# Configure project
+# ------------------------------------------------------------------------------
+
+ctest_configure(
+  BUILD "${CTEST_BINARY_DIRECTORY}"
+  SOURCE "${CTEST_SOURCE_DIRECTORY}"
+  OPTIONS
+    "-DCMAKE_INSTALL_PREFIX=kokkos-kernels_install"
+	"-DBUILD_SHARED_LIBS=ON"
+	"-DCMAKE_BUILD_TYPE=Release"
+	"-DCMAKE_VERBOSE_MAKEFILE=OFF"
+	"-DKokkos_ROOT=kokkos_install"
+	"-DKokkosKernels_ENABLE_TESTS:BOOL=ON"
+	"-DKokkosKernels_ENABLE_EXAMPLES:BOOL=ON"
+	"-DKokkosKernels_ENABLE_BENCHMARKS:BOOL=ON"
+	"-DKokkosKernels_RUN_BENCHMARKS:BOOL=ON"
+  RETURN_VALUE configure_result
+)
+
+# ------------------------------------------------------------------------------
+# Build project
+# ------------------------------------------------------------------------------
+
+ctest_build(
+  BUILD "${CTEST_BINARY_DIRECTORY}"
+  NUMBER_ERRORS build_errors
+  RETURN_VALUE build_result
+)
+
+# ------------------------------------------------------------------------------
+# Run tests
+# ------------------------------------------------------------------------------
+
+ctest_test(
+  BUILD "${CTEST_BINARY_DIRECTORY}"
+  RETURN_VALUE test_result
+)
+
+# ------------------------------------------------------------------------------
+# Submit results to CDash
+# ------------------------------------------------------------------------------
+
+ctest_submit()
+
+# ------------------------------------------------------------------------------
+# Failure handling
+# ------------------------------------------------------------------------------
+
+if(configure_result)
+  message(FATAL_ERROR "Error during configuration! Exit code: ${configure_result}")
+endif()
+
+if(build_result)
+  message(FATAL_ERROR "Error during build! Exit code: ${build_result}" with ${build_errors} errors)
+endif()
+
+if(test_result)
+  message(FATAL_ERROR "Error during testing! Exit code: ${test_result}")
+endif()
 ctest_submit()
