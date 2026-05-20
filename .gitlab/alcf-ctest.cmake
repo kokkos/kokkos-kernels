ebd724346b45fcc001d689d694848cb78f09d6f0
Luc Berger <lberge@sandia.gov>
2026-05-20 13:55:28 -0600

    Alcf build directory fix (#3108)
    * Update git command
    
    Signed-off-by: Luc Berger-Vergiat <lberge@sandia.gov>
    
    * Another path fix
    
    Signed-off-by: Luc Berger-Vergiat <lberge@sandia.gov>
    
    ---------
    
    Signed-off-by: Luc Berger-Vergiat <lberge@sandia.gov>

--- .gitlab/alcf-ctest.cmake
+++ .gitlab/alcf-ctest.cmake
@@ -6,21 +6,21 @@ set(CTEST_BUILD_NAME "Linux-g++")
 
 set(CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
 
-set(CTEST_UPDATE_COMMAND /usr/bin/git -v)
+set(CTEST_UPDATE_COMMAND "/usr/bin/git -v")
 
-set(CTEST_CONFIGURE_COMMAND "cmake -S $ENV{PWD} \
-			    -B kokkos-kernels_build \
-			    -DCMAKE_INSTALL_PREFIX=kokkos-kernels_install \
+set(CTEST_CONFIGURE_COMMAND "cmake -S ${CTEST_SOURCE_DIRECTORY} \
+			    -B ${CTEST_BINARY_DIRECTORY} \
+			    -DCMAKE_INSTALL_PREFIX=${CTEST_SOURCE_DIRECTORY}/kokkos-kernels_install \
 			    -DBUILD_SHARED_LIBS=ON \
 			    -DCMAKE_BUILD_TYPE=Release \
 			    -DCMAKE_VERBOSE_MAKEFILE=OFF \
-			    -DKokkos_ROOT=$ENV{PWD}/kokkos_install \
+			    -DKokkos_ROOT=${CTEST_SOURCE_DIRECTORY}/kokkos_install \
 			    -DKokkosKernels_ENABLE_TESTS:BOOL=ON \
 			    -DKokkosKernels_ENABLE_EXAMPLES:BOOL=ON \
 			    -DKokkosKernels_ENABLE_BENCHMARKS:BOOL=ON \
 			    -DKokkosKernels_RUN_BENCHMARKS:BOOL=ON")
 
-set(CTEST_BUILD_COMMAND "cmake --build $ENV{PWD}/kokkos-kernels_build --parallel")
+set(CTEST_BUILD_COMMAND "cmake --build ${CTEST_BINARY_DIRECTORY} --parallel")
 
 ctest_start(Nightly)
 ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}")
