581177f0371b9444603b7b604b459269625718c1
Nathan Ellingwood <ndellin@sandia.gov>
2026-07-01 20:44:38 -0600

    Fixes for out-of-rank extent calls
    Part 1 - common, sparse and blas fixes (and incidental batched fixes)
    
    Compatibility updates for KOKKOS_ENABLE_DEPRECATED_CODE_5=OFF
    See https://github.com/kokkos/kokkos/pull/9076
    
    Signed-off-by: Nathan Ellingwood <ndellin@sandia.gov>

--- common/unit_test/Test_Common_IOUtils.hpp
+++ common/unit_test/Test_Common_IOUtils.hpp
@@ -31,6 +31,7 @@ template <typename exec_space>
 void testPrintView() {
   using scalar_t   = KokkosKernels::default_scalar;
   using Unmanaged  = Kokkos::MemoryTraits<Kokkos::Unmanaged>;
+  // TODO add 0Dview printer or remove the test
   using rank0_view = Kokkos::View<scalar_t, Kokkos::HostSpace, Unmanaged>;
   using rank1_view = Kokkos::View<scalar_t *, Kokkos::HostSpace, Unmanaged>;
   using rank2_view = Kokkos::View<scalar_t **, Kokkos::HostSpace, Unmanaged>;
