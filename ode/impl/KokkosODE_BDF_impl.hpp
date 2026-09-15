ed3444de3fbbd8a9115fa71c4a0a89f704372163
Luc Berger-Vergiat <lberge@sandia.gov>
2026-09-15 10:30:04 -0600

    Revert "fix(ode): norm was not accumulating or resetting (#3276)"
    This reverts commit b621ce5c676f206470a18481b04d0e1270e3c08a.
    
    Signed-off-by: Luc Berger-Vergiat <lberge@sandia.gov>

--- ode/impl/KokkosODE_BDF_impl.hpp
+++ ode/impl/KokkosODE_BDF_impl.hpp
@@ -154,7 +154,7 @@ KOKKOS_FUNCTION void BDFStep(ode_type& ode, const table_type& table, scalar_type
     y_new(eqIdx) = y_old(eqIdx);
   }
 
-  // solve the nonlinear problem
+  // solver the nonlinear problem
   { KokkosODE::Experimental::Newton::Solve(sys, param, jac, temp, y_new, rhs, update, scale); }
 
 }  // BDFStep
