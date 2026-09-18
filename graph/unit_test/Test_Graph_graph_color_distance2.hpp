541937ef7c8a760c1fcc9a087a4a4c66a1109ab1
Brian Kelley <bmkelle@sandia.gov>
2026-09-18 13:26:10 -0600

    Formatting
    Signed-off-by: Brian Kelley <bmkelle@sandia.gov>

--- graph/unit_test/Test_Graph_graph_color_distance2.hpp
+++ graph/unit_test/Test_Graph_graph_color_distance2.hpp
@@ -300,7 +300,7 @@ void test_bipartite(lno_t numRows, lno_t numCols, size_type nnz, lno_t bandwidth
   TEST_F(TestCategory, graph##_##graph_color_distance2##_##SCALAR##_##ORDINAL##_##OFFSET##_##DEVICE) {     \
     test_dist2_coloring<SCALAR, ORDINAL, OFFSET, DEVICE>(5000, 5000 * 20, 1000, 10);                       \
     test_dist2_coloring<SCALAR, ORDINAL, OFFSET, DEVICE>(50, 50 * 10, 40, 10);                             \
-    test_dist2_coloring_clique<SCALAR, ORDINAL, OFFSET, DEVICE>(64);                                      \
+    test_dist2_coloring_clique<SCALAR, ORDINAL, OFFSET, DEVICE>(64);                                       \
     test_dist2_coloring_clique<SCALAR, ORDINAL, OFFSET, DEVICE>(130);                                      \
   }                                                                                                        \
   TEST_F(TestCategory, graph##_##graph_color_bipartite_sym##_##SCALAR##_##ORDINAL##_##OFFSET##_##DEVICE) { \
